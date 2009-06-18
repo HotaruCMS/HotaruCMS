91befcfdb760a24ec0ec7d1c19926ad87ebb6f01
longcountdown <longcountdown@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-06-18 17:46:34 +0000

    Having some bizarre issues with TortoiseSVN, but this should get the rss_sidebar file on Assembla.

---
+++ plugins/rss_sidebar/rss_sidebar.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * name: RSS Sidebar
+ * description: Adds links in the sidebar to the latest posts from a specified RSS feed.
+ * version: 0.1
+ * folder: rss_sidebar
+ * function: rss_sidebar
+ *
+ * Usage: Add <?php $plugin->check_actions('rss_sidebar'); ?> to your theme, wherever you want to show the links.
+ */
+	
+function rss_sidebar() {
+    global $hotaru;
+	    
+    /* *********** EDIT THESE SETTINGS ****************************** */
+    $feedurl = "http://hotarucms.org/external.php?type=rss2";
+    $cache = true;
+    $cache_duration = 10; // minutes
+    $max_items = 10;
+    /* ************************************************************** */
+    
+    $feed = $hotaru->new_simplepie($feedurl, $cache, $cache_duration);
+    $feed->init();
+        
+    $output = "";
+    $item_count = 0;
+	    
+    if ($feed->data) { 
+        foreach ($feed->get_items() as $item) {
+                $output .= '';
+                $output .= '<li><a href="' . $item->get_permalink() . '">' . $item->get_title() . '</a></li>';
+            $item_count++;
+            if($item_count >= $max_items) { break;}
+        }
+    }
+	
+    echo $output;
+}
+ 	
+?>
