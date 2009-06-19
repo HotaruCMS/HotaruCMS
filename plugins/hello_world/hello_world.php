dd50a5a98eb6e3e7cd7869bfae26c0920a671e83
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-06-19 05:41:37 +0000

    Minor update to comments.

--- plugins/hello_world/hello_world.php
+++ plugins/hello_world/hello_world.php
@@ -20,10 +20,10 @@ Four MUST-DO's when making a Hotaru plugin (if you want it to work!):
 
 1. You must have a folder, e.g. "hello_world"
 2. You must have a .php file with exactly the same name, e.g. "hello_world"
-3. You must include comments at the top of the file, listing name, description, version, folder and function. Use the same formatting as shown above, i.e. colon separators.
-4. You must have a function with the same name as the folder, e.g. function hello_world();
+3. You must include comments at the top of the file, listing name, description, version, folder and hooks (comma separated). Use the same formatting as shown above, i.e. colon separators.
+4. You must have a function with the same name as each hook, e.g. function hello_world();
  
-In a template, add <?php $hotaru->check_actions('FUNCTION_NAME'); ?> to call your plugin.
+In a template, add <?php $hotaru->check_actions('HOOK_NAME'); ?> to call your plugin.
 
 You can add as many files and folders to your plugin as you like, just as long as it all starts with the above steps.
 
