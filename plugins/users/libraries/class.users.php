74f7d221c2136986863d72521153cfa4322b5745
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-06-25 03:59:17 +0000

    Removed the need for the "userbase" class in Install. Moved "userbase" to the User plugin. Made sure an Admin link is present even if the User plugin is disabled or not installed.

---
+++ plugins/users/libraries/class.users.php
@@ -0,0 +1,17 @@
+<?php
+
+/* ******************************************************************** 
+ *  File: plugins/users/libraries/class.users.php
+ *  Purpose: Everything pertaining to users
+ *  Notes: ---
+ ********************************************************************** */
+ 
+class Users extends UserBase {	// functions dealing with multiple users
+
+}
+
+class User extends Users {	// functions dealing with multiple users
+
+}
+
+?>
