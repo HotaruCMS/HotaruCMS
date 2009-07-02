56015ea8dee4a40846be64b2ac2db7b98a5a38fa
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-02 11:40:49 +0000

    I've started a "Submit" plugin which creates tables for "posts" and allows you to submit links with custom titles, which are displayed most recent first on the front page. Extending this with descriptions, tags, categories, etc. shouldn't be too much of a challenge.

---
+++ alpha/plugins/submit/class.post.php
@@ -0,0 +1,64 @@
+<?php
+
+/* ********** PLUGIN CLASSES**************************************************************************
+ * name: Post
+ * description: Class for functions related to submitting and organizing posts
+ * file: /plugins/submit/libraries/class.post.php
+ *
+ *  License:
+ *
+ *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *
+ *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the 
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of 
+ *   the License, or (at your option) any later version.
+ *
+ *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not, 
+ *   see http://www.gnu.org/licenses/.
+ *   
+ *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
+ *
+ **************************************************************************************************** */
+ 
+class Post {	
+
+	var $source_url = '';
+	var $post_title = '';
+	var $post_status = 'processing';
+
+
+	/* ******************************************************************** 
+	 *  Function: add_post
+	 *  Parameters: None
+	 *  Purpose: Adds a post to the database
+	 *  Notes: ---
+	 ********************************************************************** */	
+	 
+	function add_post() {
+		global $db;
+		$sql = "INSERT INTO " . table_posts . " SET post_orig_url = %s, post_title = %s, post_status = %s";
+		$db->query($db->prepare($sql, $this->source_url, $this->post_title, $this->post_status));
+		return true;
+	}
+
+
+	/* ******************************************************************** 
+	 *  Function: get_posts
+	 *  Parameters: None
+	 *  Purpose: Gets the last X posts from the database
+	 *  Notes: ---
+	 ********************************************************************** */	
+	 	
+	function get_posts($limit = 20) {
+		global $db;
+		$sql = "SELECT * FROM " . table_posts . " ORDER BY post_date DESC LIMIT %d";
+		$posts = $db->get_results($db->prepare($sql, $limit));
+		if($posts) { return $posts; } else { return false; }
+	}
+}
+
+?>
