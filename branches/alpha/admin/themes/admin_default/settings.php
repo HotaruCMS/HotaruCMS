a38242ee2f9b27cef3f79488bec1ca8c4059f977
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-04 11:45:06 +0000

    Moved blocks of php out of admin templates and into the class.admin.php file.

--- branches/alpha/admin/themes/admin_default/settings.php
+++ branches/alpha/admin/themes/admin_default/settings.php
@@ -24,69 +24,44 @@
  *
  **************************************************************************************************** */
 
-global $hotaru, $plugin, $admin, $cage, $lang; // don't remove
+global $plugin, $admin, $cage; // don't remove
 ?>
 
 <h2><a href="<?php echo url(array(), 'admin'); ?>"><?php echo site_name;?> Admin Control Panel</a> &raquo; Settings</h2>
 
-<?php 
-	
-	$plugin->check_actions('admin_settings_top'); 
-
-	$loaded_settings = $admin->get_all_admin_settings();	// get all admin settings from the database
-	
-	$error = 0;
-	
-	if($cage->post->noTags('settings_update')  == 'true') {
-		foreach($loaded_settings as $setting_name) {
-			if($cage->post->keyExists($setting_name->settings_name)) {
-				$setting_value = $cage->post->noTags($setting_name->settings_name);
-				if($setting_value && $setting_value != $setting_name->settings_value) {
-					$admin->admin_setting_update($setting_name->settings_name, $setting_value);
-
-				} else {
-					if(!$setting_value) {
-						// empty value 
-						$error = 1; 
-					} else { 
-						// No change to the value
-						$error = 0; 
-					}
-				}
-			} else {
-				// error, setting doesn't exist.
-				$error = 1;
-			}
-		}
-		
-		if($error == 0) {
-			$plugin->message = $lang['admin_settings_update_success'];
-			$plugin->message_type = 'green';
-			$plugin->show_message();		
-		} else {
-			$plugin->message = $lang['admin_settings_update_failure'];
-			$plugin->message_type = 'red';
-			$plugin->show_message();
-		}
-	}
-	
-	// Reload all the settings to display the most up-to-date form.
-	$loaded_settings = $admin->get_all_admin_settings();	// get all admin settings from the database
-	
-?>
+<?php $plugin->check_actions('admin_settings_top'); ?>
 	
 	<h1>Hotaru Settings</h1>
+	
+	<?php $loaded_settings = $admin->settings();	// Prepare or process the form ?>
+	
 	<form id='settings_form' name='settings_form' action='<?php echo baseurl; ?>admin/admin_index.php?page=settings' method='post'>
 	
 	<table id="settings">	
-	<tr><td><b><u>Setting</u></b></td><td><b><u>Value</u></b></td><td><b><u>Default</u></b></td><td><b><u>Notes</u></b></td></tr>
-	<?php foreach($loaded_settings as $ls) { 
-		$name = ucfirst(preg_replace('/_/', ' ', $ls->settings_name));
-	?>
 	<tr>
-	<td><?php echo $name; ?>: </td><td><input type='text' size=20 name='<?php echo $ls->settings_name; ?>' value='<?php echo $ls->settings_value; ?>' /></td><td><?php echo $ls->settings_default; ?></td><td><i><?php echo $ls->settings_note; ?></i></td>
+		<td><b><u>Setting</u></b></td>
+		<td><b><u>Value</u></b></td>
+		<td><b><u>Default</u></b></td>
+		<td><b><u>Notes</u></b></td>
 	</tr>
-	<?php } ?>
+	
+	<?php 	// **********************************************************
+	
+		// Loop through the settings, displaying each one as a row...	
+		foreach($loaded_settings as $ls) { 
+		
+			// replace underscores with spaces and make the first character of the setting name uppercase.
+			$name = ucfirst(preg_replace('/_/', ' ', $ls->settings_name));	
+		?>
+			<tr>
+			<td><?php echo $name; ?>: </td>
+			<td><input type='text' size=20 name='<?php echo $ls->settings_name; ?>' value='<?php echo $ls->settings_value; ?>' /></td>
+			<td><?php echo $ls->settings_default; ?></td>
+			<td><i><?php echo $ls->settings_note; ?></i></td>
+			</tr>
+	 
+	<?php 	} // End loop ********************************************************** 	?>
+	
 	<br />
 	<input type='hidden' name='settings_update' value='true' />
 	</table>
