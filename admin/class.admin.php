98b2dcf5c15192ed4a9d46da2bc7900b3fdee122
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-05 16:52:03 +0000

    Minor bug fix.

--- admin/class.admin.php
+++ admin/class.admin.php
@@ -43,8 +43,8 @@ class Admin {
 		/* First check if there's a specified plugin for the file and load 
 		   the template from the plugin folder if it's there. */
 		if($plugin != '') {
-			if(file_exists(plugins .  $plugin . $page)) {
-				include_once(plugins . $plugin . $page);
+			if(file_exists(plugins .  $plugin . '/' . $page)) {
+				include_once(plugins . $plugin . '/' . $page);
 				return;
 			}
 		}
