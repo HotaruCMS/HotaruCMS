5927148f61224033d99b3bceb2eba8f1e6dcb9f4
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-12 07:33:07 +0000

    Fix for problems with multibyte characters.

---
+++ alpha/plugins/submit/submit_form_2.php
@@ -0,0 +1,214 @@
+<?php
+ 
+/* **************************************************************************************************** 
+ *  File: /plugins/submit/submit_form.php
+ *  Purpose: Step 2 for submitting a new story.
+ *  Notes: This file is part of the Submit plugin. The main file is /plugins/submit/submit.php
+ *  License:
+ *
+ *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *
+ *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the 
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of 
+ *   the License, or (at your option) any later version.
+ *
+ *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not, 
+ *   see http://www.gnu.org/licenses/.
+ *   
+ *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
+ *
+ **************************************************************************************************** */
+ 
+
+ /* ******************************************************************** 
+ *  Function: sub_submit_form_2
+ *  Parameters: None
+ *  Purpose: Step 2 - Enter title, description, tags, etc.
+ *  Notes: ---
+ ********************************************************************** */
+ 
+function sub_submit_form_2($post_orig_url, $post_orig_title) {
+	global $hotaru, $cage, $lang, $post;
+		
+	echo "<div id='main'>";
+		echo "<h2><a href='" . baseurl . "'>Home</a> &raquo; Submit a Story 2/2</h2>\n";
+			
+		$hotaru->show_message();
+				
+		echo "<div class='main_inner'>";
+		echo $lang["submit_form_instructions_2"] . "\n";
+		
+		$error = 0;
+		
+		if($cage->post->getAlpha('submit2') == 'true') {
+			if(!sub_check_for_errors_2()) { 
+				return true; // No errors found, return true.
+			} else {
+				$title_check = $cage->post->noTags('post_title');	
+				$content_check = $cage->post->noTags('post_content');	
+				$tags_check = $cage->post->noTags('post_tags');
+			}
+		} else {
+			$title_check = $post_orig_title;
+			$content_check = "";
+			$tags_check = "";
+		}
+		
+	
+			echo "<form name='submit_form_2' action='" . baseurl . "index.php?page=submit2&sourceurl=" . $post_orig_url . "' method='post'>\n";
+			echo "<table>";
+?>
+			
+			<tr>
+				<td><?php echo $lang["submit_form_url"] ?>:&nbsp; </td>
+				<td><?php echo $post_orig_url; ?></td>
+				<td>&nbsp;</td>
+			</tr>
+
+
+
+
+			<tr>
+				<td><?php echo $lang["submit_form_title"] ?>:&nbsp; </td>
+				<td><input type='text' size=50 id='post_title' name='post_title' value='<?php echo $title_check ?>'></td>
+				<td id='ajax_loader'>&nbsp;</td>
+			</tr>
+			
+			
+			
+			
+			<?php if($post->use_content) { ?>
+			<tr>
+				<td style='vertical-align: top;'><?php echo $lang["submit_form_content"] ?>:&nbsp; </td>
+				<td colspan='2'><textarea id='post_content' name='post_content' rows='6' maxlength='<?php $post->post_content_length; ?>' style='width: 32em;'><?php echo $content_check ?></textarea></td>
+			</tr>
+			<?php } ?>
+			
+			
+			
+			
+			<?php if($post->use_tags) { ?>
+			<tr>
+				<td><?php echo $lang["submit_form_tags"] ?>:&nbsp; </td>
+				<td><input type='text' size=50 name='post_tags' value='<?php echo $tags_check ?>'></td>
+				<td>&nbsp;</td>
+			</tr>
+			<?php } ?>
+			
+			
+			
+			<input type='hidden' name='submit2' value='true' />
+
+<?php				
+			echo "<tr><td colspan=3>&nbsp;</td></tr>";
+			echo "<tr><td>&nbsp; </td><td>&nbsp; </td><td style='text-align:right;'><input type='submit' name='submit' value='" . $lang['submit_form_submit_button'] . "' /></td></tr>\n";	
+			echo "</table>\n";
+			echo "</form>\n";
+		echo "</div>\n";
+	echo "</div>\n";
+}
+
+
+/* ******************************************************************** 
+ *  Function: sub_check_for_errors_2
+ *  Parameters: None
+ *  Purpose: Checks submit_form_2 for errors
+ *  Notes: ---
+ ********************************************************************** */
+
+function sub_check_for_errors_2() {
+	global $hotaru, $post, $cage, $lang;
+
+	// ******** CHECK TITLE ********
+	
+	$title_check = $cage->post->noTags('post_title');	
+	if(!$title_check) {
+		// No title present...
+		$hotaru->message = $lang['submit_form_title_not_present_error'];
+		$hotaru->message_type = 'red';
+		$hotaru->show_message();
+		$error = 1;
+	} elseif($post->title_exists($title_check )) {
+		// URL already exists...
+		$hotaru->message = $lang['submit_form_title_already_exists_error'];
+		$hotaru->message_type = 'red';
+		$hotaru->show_message();
+		$error = 1;
+	} else {
+		// title is okay.
+		$error = 0;
+	}
+	
+	// ******** CHECK DESCRIPTION ********
+	if($post->use_content) {
+		$content_check = $cage->post->noTags('post_content');	
+		if(!$content_check) {
+			// No content present...
+			$hotaru->message = $lang['submit_form_content_not_present_error'];
+			$hotaru->message_type = 'red';
+			$hotaru->show_message();
+			$error = 1;
+		} elseif(strlen($content_check) < $post->post_content_length) {
+			// content is too short
+			$hotaru->message = $lang['submit_form_content_too_short_error'];
+			$hotaru->message_type = 'red';
+			$hotaru->show_message();
+			$error = 1;
+		} else {
+			// content is okay.
+			$error = 0;
+		}
+	}
+	
+	// ******** CHECK TAGS ********
+	if($post->use_tags) {
+		$tags_check = $cage->post->noTags('post_tags');	
+		if(!$tags_check) {
+			// No content present...
+			$hotaru->message = $lang['submit_form_tags_not_present_error'];
+			$hotaru->message_type = 'red';
+			$hotaru->show_message();
+			$error = 1;
+		} elseif(strlen($tags_check) > $post->post_max_tags) {
+			// tags are too long
+			$hotaru->message = $lang['submit_form_tags_length_error'];
+			$hotaru->message_type = 'red';
+			$hotaru->show_message();
+			$error = 1;
+		} else {
+			// tags are okay.
+			$error = 0;
+		}
+	}
+	
+	// Return true if error is found
+	if($error == 1) { return true; } else { return false; }
+}
+
+
+/* ******************************************************************** 
+ *  Function: sub_process_submission
+ *  Parameters: None
+ *  Purpose: Saves the submitted story to the database
+ *  Notes: ---
+ ********************************************************************** */
+ 
+function sub_process_submission($post_orig_url) {
+	global $hotaru, $cage, $current_user, $post;
+		
+	$post->post_orig_url = $post_orig_url;
+	$post->post_url = $cage->post->getFriendlyUrl('post_title');
+	$post->post_title = $cage->post->getMixedString2('post_title');
+	$post->post_content = $cage->post->getMixedString2('post_content');
+	$post->post_tags = $cage->post->getMixedString2('post_tags');
+	$post->post_status = "new";
+	$post->post_author = $current_user->id;
+	$post->add_post();
+
+}
+
+?>
