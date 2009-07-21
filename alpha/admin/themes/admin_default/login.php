f71bed57b79cf22d97cec5eac5838184dbc8a944
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-21 11:54:23 +0000

    Moved the Admin login form out of the core and into the admin_default theme.

--- alpha/admin/themes/admin_default/login.php
+++ alpha/admin/themes/admin_default/login.php
@@ -2,7 +2,7 @@
 
 /* ******* ADMIN TEMPLATE ************************************************************************** 
  * Theme name: admin_default
- * Template name: main.php
+ * Template name: login.php
  * Template author: Nick Ramsay
  * Version: 0.1
  * License:
@@ -30,13 +30,30 @@ global $hotaru, $lang; // don't remove
 <p class="breadcrumbs">
 	<a href="<?php echo baseurl; ?>"><?php echo site_name?></a> 
 	&raquo; <a href="<?php echo url(array(), 'admin'); ?>"><?php echo $lang["admin_theme_main_admin_cp"]?></a> 
-	&raquo; <?php echo $lang["admin_theme_main_admin_home"]?>
+	&raquo; <?php echo $lang["admin_theme_login"]; ?>
 </p>
-
-<!-- TITLE FOR ADMIN NEWS -->
-<h2>
-	<a href="http://feeds2.feedburner.com/hotarucms"><img src="<?php echo baseurl ?>admin/themes/admin_default/images/rss_16.gif"></a>
-	&nbsp;<?php echo $lang["admin_theme_main_latest"]?>
-</h2>
-
-<?php echo admin_news(); ?>
+		
+<?php $hotaru->show_message(); ?>
+		
+<div class='main_inner'>
+	<?php echo $lang["admin_theme_login_instructions"] ?>
+	
+	<form name='login_form' action='<?php echo baseurl ?>admin/admin_index.php' method='post'>	
+	<table>
+		<tr>
+		<td><?php echo $lang["admin_theme_login_username"] ?>:&nbsp; </td>
+		<td><input type='text' size=30 name='username' value='<?php echo $username_check ?>' /></td>
+		</tr>
+		<tr>
+		<td><?php echo $lang["admin_theme_login_password"] ?>:&nbsp; </td>
+		<td><input type='password' size=30 name='password' value='<?php echo $password_check ?>' /></td>
+		</tr>
+		<tr>
+		<td>&nbsp; </td>
+		<td style='text-align:right;'><input type='submit' value='<?php echo $lang['admin_theme_login_form_submit'] ?>' /></td>
+		</tr>			
+	</table>
+	<input type='hidden' name='login_attempted' value='true'>
+	<input type='hidden' name='page' value='admin_login'>
+	</form>
+</div>
