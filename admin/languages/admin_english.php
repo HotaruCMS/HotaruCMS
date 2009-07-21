b943cd8370c68150f6b607660f0d2f4dbfcc4066
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-21 12:09:37 +0000

    Moved admin theme language into the admin theme folder to give theme designers control of the language. It will be interesting to see people design custom Admin themes.

--- admin/languages/admin_english.php
+++ admin/languages/admin_english.php
@@ -1,9 +1,7 @@
 <?php
 
-// ADMIN
-
 /* **************************************************************************************************** 
- *  File: /languages/admin/admin_english.php
+ *  File: /admin/languages/admin_english.php
  *  Purpose: A language file for English. It's used whenever an admin file needs to output language.
  *  Notes: ---
  *  License:
@@ -25,89 +23,12 @@
  *
  **************************************************************************************************** */
 
-/* ***************************************************************** 
-******************************************************************** 
-******************************************************************** 
-**********						  **********
-**********		ADMIN THEME LANGUAGE 		  **********
-**********						  **********
-******************************************************************** 
-******************************************************************** 
-***************************************************************** */
-
-/* login */
-$lang["admin_theme_login"] = "Login";
-$lang["admin_theme_login_username"] = "Username";
-$lang["admin_theme_login_password"] = "Password";
-$lang["admin_theme_login_instructions"] = "Enter your username and password:";
-$lang["admin_theme_login_form_submit"] = "Login";
-
-/* navigation */
-$lang["admin_theme_navigation_home"] = "Home";
-$lang["admin_theme_navigation_admin"] = "Admin";
-$lang["admin_theme_navigation_login"] = "Login";
-$lang["admin_theme_navigation_logout"] = "Logout";
-
-/* main */
-$lang["admin_theme_main_admin_cp"] = "Admin Control Panel";
-$lang["admin_theme_main_admin_home"] = "Admin Home";
-$lang["admin_theme_main_latest"] = "Latest from Hotaru CMS";
-
-/* maintenance */
-$lang["admin_theme_maintenance"] = "Maintenance";
-$lang["admin_theme_maintenance_title"] = "Hotaru Maintenance";
-$lang["admin_theme_maintenance_db_cache"] = "Clear database cache";
-$lang["admin_theme_maintenance_db_cache_desc"] = "deletes cached database queries.";
-$lang["admin_theme_maintenance_rss_cache"] = "Clear RSS cache";
-$lang["admin_theme_maintenance_rss_cache_desc"] = "deletes cached RSS feeds.";
-
-/* plugins */
-$lang["admin_theme_plugins"] = "Plugin Management";
-$lang["admin_theme_plugins_installed"] = "Installed";
-$lang["admin_theme_plugins_not_installed"] = "Not installed";
-$lang["admin_theme_plugins_active"] = "Active";
-$lang["admin_theme_plugins_switch"] = "Switch";
-$lang["admin_theme_plugins_plugin"] = "Plugin";
-$lang["admin_theme_plugins_install"] = "Install";
-$lang["admin_theme_plugins_order"] = "Order";
-$lang["admin_theme_plugins_details"] = "Details";
-$lang["admin_theme_plugins_requires"] = "Requires";
-$lang["admin_theme_plugins_description"] = "Description";
-$lang["admin_theme_plugins_close"] = "Close";
-$lang["admin_theme_plugins_no_plugins"] = "No additional plugins needed.";
-$lang["admin_theme_plugins_guide"] = "Plugin Management Guide";
-$lang["admin_theme_plugins_guide1"] = "The order column is used to determine which plugins are checked for hooks first.";
-$lang["admin_theme_plugins_guide2"] = "Uninstalling a plugin will delete it from the <i>plugins</i>, <i>pluginhooks</i> and <i>pluginsettings</i> tables.";
-$lang["admin_theme_plugins_guide3"] = "Any other database entries created by the plugin will not be removed.";
-
-/* settings */
-$lang["admin_theme_settings"] = "Settings";
-$lang["admin_theme_settings_title"] = "Hotaru Settings";
-$lang["admin_theme_settings_setting"] = "Setting";
-$lang["admin_theme_settings_value"] = "Value";
-$lang["admin_theme_settings_default"] = "Default";
-$lang["admin_theme_settings_notes"] = "Notes";
-
-/* plugin settings */
-$lang["admin_theme_plugin_settings"] = "Plugin Settings";
-
-/* footer */
-$lang["admin_theme_footer_having_trouble_vist_forums"] = "Having trouble? Visit the forums at";
-$lang["admin_theme_footer_for_help"] = "for help.";
-
-/* 404 */
-$lang["admin_theme_404_page_not_found"] = "Page not found.";
-
-
-
 
 /* ***************************************************************** 
-******************************************************************** 
 ******************************************************************** 
 **********						  **********
 **********		ADMIN CORE LANGUAGE 		  **********
 **********						  **********
-******************************************************************** 
 ******************************************************************** 
 ***************************************************************** */
 
