46c1ee446b6d82ef0c800fde8d8ed1b0583becec
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-21 14:46:07 +0000

    Moved main language file to custom folder.

--- hotaru_settings.php
+++ hotaru_settings.php
@@ -49,7 +49,7 @@ define('baseurl', "http://localhost/hotaru/branches/alpha/");	//e.g. http://www.
 
 // define shorthand paths
 define("includes", dirname(__FILE__).'/3rd_party/');
-define("languages", dirname(__FILE__).'/languages/');
+define("languages", dirname(__FILE__).'/custom/core_language/');
 define("functions", dirname(__FILE__).'/functions/');
 define("plugins", dirname(__FILE__).'/plugins/');
 define("install", dirname(__FILE__).'/install/');
