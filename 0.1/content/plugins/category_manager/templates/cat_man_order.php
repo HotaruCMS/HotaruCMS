3c7842c8e52abbf584f69ae9b6664a4a498bf055
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-07-30 13:23:07 +0000

    Renaming alpha branch as 0.1.

---
+++ 0.1/content/plugins/category_manager/templates/cat_man_order.php
@@ -0,0 +1,55 @@
+<?php
+
+/* ******* PLUGIN TEMPLATE ************************************************************************** 
+ * Plugin name: Category Manager
+ * Template name: plugins/category_manager/cat_man_order.php
+ *  License:
+ *
+ *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *
+ *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the 
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of 
+ *   the License, or (at your option) any later version.
+ *
+ *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not, 
+ *   see http://www.gnu.org/licenses/.
+ *   
+ *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
+ *
+ **************************************************************************************************** */
+ 
+ global $hotaru, $lang, $the_cats;
+ ?>
+ 
+	<h2><?php echo $lang["cat_man_order"] ?></h2>
+
+	<table class="cat_man_table">
+	<tr><td class="cat_man_body">
+		<?php echo $lang["cat_man_order_instruct"] ?><br />
+
+		<h3>1. <a href="<?php echo baseurl ?>admin/admin_index.php?page=plugin_settings&amp;plugin=category_manager&amp;action=order_alpha"><?php echo $lang["cat_man_order_alpha"] ?></a></h3>
+		<?php echo $lang["cat_man_order_alpha_desc"] ?> <br />
+		<h3>2. <a href="<?php echo baseurl ?>admin/admin_index.php?page=plugin_settings&amp;plugin=category_manager&amp;action=order_length"><?php echo $lang["cat_man_order_length"] ?></a></h3>
+		<?php echo $lang["cat_man_order_length_desc"] ?>  <br />
+		<h3>3. <a href="<?php echo baseurl ?>admin/admin_index.php?page=plugin_settings&amp;plugin=category_manager&amp;action=order_id"><?php echo $lang["cat_man_order_id"] ?></a></h3>
+		<?php echo $lang["cat_man_order_id_desc"] ?> <br />
+		<h3>4. <a href="<?php echo baseurl ?>admin/admin_index.php?page=plugin_settings&amp;plugin=category_manager&amp;action=order_posts"><?php echo $lang["cat_man_order_posts"] ?></a></h3>
+		<?php echo $lang["cat_man_order_posts_desc"] ?>
+
+	</td>
+	
+	<td class="cat_man_menu_holder">
+		<div>
+			<h3><?php echo $lang["cat_man_menu_title"] ?></h3>
+			<?php $hotaru->display_template('cat_man_menu', 'category_manager'); ?>
+			
+			<h3><?php echo $lang["cat_man_category_tree"] ?></h3>
+			<?php cat_man_tree($the_cats); ?>
+
+		</div>
+	</td>
+	</tr></table>
