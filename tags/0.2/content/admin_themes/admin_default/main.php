e73ffb72ec99a5fa4d6c06cb7d2d8ea7fae0c42e
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-08-07 18:04:50 +0000

    Creating a tag for Hotaru 0.2 Alpha.

---
+++ tags/0.2/content/admin_themes/admin_default/main.php
@@ -0,0 +1,42 @@
+<?php 
+
+/* ******* ADMIN TEMPLATE ************************************************************************** 
+ * Theme name: admin_default
+ * Template name: main.php
+ * Template author: Nick Ramsay
+ * Version: 0.1
+ * License:
+ *
+ *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *
+ *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the 
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of 
+ *   the License, or (at your option) any later version.
+ *
+ *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not, 
+ *   see http://www.gnu.org/licenses/.
+ *   
+ *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
+ *
+ **************************************************************************************************** */
+
+global $hotaru, $lang; // don't remove
+?>
+
+<p class="breadcrumbs">
+	<a href="<?php echo baseurl; ?>"><?php echo site_name?></a> 
+	&raquo; <a href="<?php echo url(array(), 'admin'); ?>"><?php echo $lang["admin_theme_main_admin_cp"]?></a> 
+	&raquo; <?php echo $lang["admin_theme_main_admin_home"]?>
+</p>
+
+<!-- TITLE FOR ADMIN NEWS -->
+<h2>
+	<a href="http://feeds2.feedburner.com/hotarucms"><img src="<?php echo baseurl ?>content/admin_themes/<?php echo admin_theme ?>images/rss_16.png"></a>
+	&nbsp;<?php echo $lang["admin_theme_main_latest"]?>
+</h2>
+
+<?php echo admin_news(); ?>
