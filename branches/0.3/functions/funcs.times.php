3bdde8e1a535fc0ec950e241ba67a08894af4526
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-08-12 03:09:34 +0000

    Code cleanup for function files.

--- branches/0.3/functions/funcs.times.php
+++ branches/0.3/functions/funcs.times.php
@@ -1,120 +1,145 @@
 <?php
-
-/* **************************************************************************************************** 
- *  File: /funcs.times.php
- *  Purpose: A collection of functions to deal with time.
- *  Notes: ---
- *  License:
+/**
+ * A collection of functions to deal with time
  *
- *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ * PHP version 5
  *
- *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the 
- *   GNU General Public License as published by the Free Software Foundation, either version 3 of 
- *   the License, or (at your option) any later version.
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
  *
- *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *   GNU General Public License for more details.
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
  *
- *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not, 
- *   see http://www.gnu.org/licenses/.
- *   
- *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
- *
- **************************************************************************************************** */
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
  
  
- /* ******************************************************************** 
- *  Function: timer_start
- *  Parameters: None
- *  Purpose: Starts the timer, for debugging purposes.
- *  Notes: Function borrowed from Wordpress.org
- ********************************************************************** */
- 
-function timer_start() {
-	global $timestart;
-	$mtime = explode(' ', microtime() );
-	$mtime = $mtime[1] + $mtime[0];
-	$timestart = $mtime;
-	return true;
+ /**
+ * Start timer for calculating page loading time
+ *
+ * @return true
+ *
+ * Note: Function borrowed from Wordpress.org
+ */
+function timer_start()
+{
+    global $timestart;
+    
+    $mtime = explode(' ', microtime() );
+    $mtime = $mtime[1] + $mtime[0];
+    $timestart = $mtime;
+    return true;
 }
 
 
- /* ******************************************************************** 
- *  Function: timer_stop
- *  Parameters: Precision - no. of digits after the decimal point
- *  Purpose: Stops the debugging timer.
+ /**
+ * Stop the timer
+ *
+ * @param int $precision number of digits after the decimal point
+ * @return int $timetotal total timed
+ * 
  *  Notes: Measured in seconds / Function borrowed from Wordpress.org
- ********************************************************************** */
-
-function timer_stop($precision = 3) { //if called like timer_stop(1), will echo $timetotal
-	global $timestart, $timeend;
-	$mtime = microtime();
-	$mtime = explode(' ',$mtime);
-	$mtime = $mtime[1] + $mtime[0];
-	$timeend = $mtime;
-	$timetotal = $timeend-$timestart;
-	$r = ( function_exists('number_format_i18n') ) ? number_format_i18n($timetotal, $precision) : number_format($timetotal, $precision);
-	return $r;
+ */
+function timer_stop($precision = 3)
+{
+    //if called like timer_stop(1), will echo $timetotal
+    
+    global $timestart, $timeend;
+    
+    $mtime = microtime();
+    $mtime = explode(' ',$mtime);
+    $mtime = $mtime[1] + $mtime[0];
+    $timeend = $mtime;
+    $timetotal = $timeend-$timestart;
+    $r = (function_exists('number_format_i18n')) ? 
+        number_format_i18n($timetotal, $precision) : 
+        number_format($timetotal, $precision);
+    return $r;
 }
 
 
- /* ******************************************************************** 
- *  Function: time_difference
- *  Parameters: "from" time
- *  Purpose: To show how long ago a post was submitted
- *  Notes: Adapted from Pligg & SWCMS' txt_time_diff() function
- ********************************************************************** */
- 
-function time_difference($from){
-	global $lang;
-	
-	$output = '';
-	$now = time();
-	$diff=$now-$from;
-	$days=intval($diff/86400);
-	$diff=$diff%86400;
-	$hours=intval($diff/3600);
-	$diff=$diff%3600;
-	$minutes=intval($diff/60);
+ /**
+ * Calculate time difference between now and a given time
+ *
+ * @param string $from time
+ * @return string
+ *
+ *  Note: Adapted from Pligg & SWCMS' txt_time_diff() function
+ */
+function time_difference($from)
+{
+    global $lang;
+    
+    $output     = '';
+    $now        = time();
+    $diff       = $now-$from;
+    $days       = intval($diff/86400);
+    $diff       = $diff%86400;
+    $hours      = intval($diff/3600);
+    $diff       = $diff%3600;
+    $minutes    = intval($diff/60);
 
-	if($days>1) $output .= $days . " " . $lang['main_times_days'] . " ";
-	elseif ($days==1) $output .= $days . " " . $lang['main_times_day'] . " ";
+    if ($days>1) {
+        $output .= $days . " " . $lang['main_times_days'] . " ";
+    } elseif ($days==1) { 
+        $output .= $days . " " . $lang['main_times_day'] . " ";
+    }
 
-	if($days < 2){
-		if($hours>1) $output .= $hours . " " . $lang['main_times_hours'] . " ";
-		else if ($hours==1) $output .= $hours . " " . $lang['main_times_hour'] . " ";
-	
-		if($hours < 3){
-			if($minutes>1) $output .= $minutes . " " . $lang['main_times_minutes'] . " ";
-			else if ($minutes==1) $output .= $minutes . " " . $lang['main_times_minute'] . " ";
-		}
-	}
-	
-	if($output=='') $output = $lang['main_times_seconds'] . " ";
-	return $output;
+    if ($days < 2){
+        if ($hours>1) {
+            $output .= $hours . " " . $lang['main_times_hours'] . " ";
+        } elseif ($hours==1) $output .= $hours . " " . $lang['main_times_hour'] . " ";
+    
+        if ($hours < 3){
+            if ($minutes>1) $output .= $minutes . " " . $lang['main_times_minutes'] . " ";
+            elseif ($minutes==1) $output .= $minutes . " " . $lang['main_times_minute'] . " ";
+        }
+    }
+    
+    if ($output=='') $output = $lang['main_times_seconds'] . " ";
+    return $output;
 }
 
 
- /* ******************************************************************** 
- *  Function: unixtimestamp
- *  Parameters: a timestamp
- *  Purpose: converts a timestamp into a number
- *  Notes: Borrowed from Pligg & SWCMS
- ********************************************************************** */
- 
-function unixtimestamp($timestamp){
-	if(strlen($timestamp) == 14) {
-		$time = substr($timestamp,0,4)."-".substr($timestamp,4,2)."-".substr($timestamp,6,2);
-		$time .= " ";
-		$time .=  substr($timestamp,8,2).":".substr($timestamp,10,2).":".substr($timestamp,12,2);
-		return strtotime($time);
-	} else {
-		if(strlen($timestamp) == 0) {
-			return 0;
-		} else {
-			return strtotime($timestamp);
-		}
-	}
+ /**
+ * Converts a timestamp into a number
+ *
+ * @param string $timestamp
+ * @return string
+ *
+ * Note: Borrowed from Pligg & SWCMS
+ */
+function unixtimestamp($timestamp)
+{
+    if (strlen($timestamp) == 14)
+    {
+        $time = substr($timestamp,0,4)."-".
+                    substr($timestamp,4,2)."-".
+                    substr($timestamp,6,2);
+        $time .= " ";
+        $time .=  substr($timestamp,8,2).":".
+                    substr($timestamp,10,2).":".
+                    substr($timestamp,12,2);
+        return strtotime($time);
+    } 
+    else 
+    {
+        if (strlen($timestamp) == 0) {
+            return 0;
+        } else {
+            return strtotime($timestamp);
+        }
+    }
 }
 ?>
