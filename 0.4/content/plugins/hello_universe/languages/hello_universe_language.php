7fea655331b7bea4e3abd7b430e3f7f15db5d86e
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-08-13 10:44:04 +0000

    Starting a 0.4 branch.

---
+++ 0.4/content/plugins/hello_universe/languages/hello_universe_language.php
@@ -0,0 +1,43 @@
+<?php
+
+/* **************************************************************************************************** 
+ *  File: plugins/hello_universe/languages/hello_universe_language.php
+ *  Purpose: A language file for Hello Universe.
+ *  Notes: ---
+ *  License:
+ *
+ *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *
+ *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the 
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of 
+ *   the License, or (at your option) any later version.
+ *
+ *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not, 
+ *   see http://www.gnu.org/licenses/.
+ *   
+ *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
+ *
+ **************************************************************************************************** */
+
+/* Front page */
+$lang["hello_universe"] = "Hello Universe!";
+$lang["hello_universe_explanation"] = "This text is shown by including the <i>theme_index_main</i> hook. See Functions #1 and #2 in hello_universe.php";
+$lang["hello_universe_see_page"] = "See a theme page made by this plugin";
+$lang["hello_universe_see_form"] = "See an example form made with this plugin";
+
+/* Example Form */
+$lang["hello_universe_form_example"] = "Example Form";
+$lang["hello_universe_question"] = "What's the capital of France?";
+$lang["hello_universe_answer"] = "Answer:";
+$lang["hello_universe_success"] = "Correct!";
+$lang["hello_universe_failure"] = "Wrong answer!";
+$lang["hello_universe_form_submit"] = "Submit";
+
+/* Common language */
+$lang["hello_universe_back_home"] = "Back Home";
+
+?>
