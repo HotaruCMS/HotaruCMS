cfa9bff8baa49e8b03d90c1915e4ab9c51b4e0c5
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-08-16 16:50:48 +0000

    Cleaned up post filtering while adding breadcrumbs.

--- 0.4/content/plugins/submit/templates/post_page.php
+++ 0.4/content/plugins/submit/templates/post_page.php
@@ -28,61 +28,63 @@ global $hotaru, $plugin, $post, $userbase, $lang;
 
 $userbase = new UserBase();
 $post->template_name = "post_page";
-
 ?>
 
-<?php if($post->post_status != 'buried') { ?>
+<!-- BREADCRUMBS -->
+<div id="breadcrumbs">
+    <a href="<?php echo baseurl; ?>"><?php echo $lang['submit_form_home']; ?></a> &raquo; 
+    <?php $plugin->check_actions('breadcrumbs'); ?> 
+    <?php echo $hotaru->title ?>
+</div>
 
-	<!-- ************ POST **************** -->
-	
-	<?php $result = $plugin->check_actions('submit_pre_show_post'); 
-		if(!isset($result) || !is_array($result)) {
-		// if buried during that plugin call, the post won't show...
-	?>
-	
-		<div class="show_post vote_button_space_<?php echo $post->post_vars['vote_type'] ?>">
-		
-			<div class="show_post_title"><a href='<?php echo $post->post_orig_url; ?>'><?php echo $post->post_title; ?></a></div>
-		
-			<?php if($post->use_author || $post->use_date) { ?>
-				<div class="show_post_author_date">	
-					Posted
-					<?php if($post->use_author) { 
-						echo " by <a href='" . url(array('user' => $userbase->get_username($post->post_author))) . "'>" . $userbase->get_username($post->post_author) . "</a>"; } 
-					?>
-					<?php if($post->use_date) { echo time_difference(unixtimestamp($post->post_date)) . " ago"; } ?>
-					<?php $plugin->check_actions('submit_show_post_author_date'); ?>
-				</div>
-			<?php } ?>
-				
-			<?php if($post->use_content) { ?>
-				<div class="show_post_content"><?php echo $post->post_content; ?></div>
-			<?php } ?>
-			
-			<div class="show_post_extra_fields">
-				<?php $plugin->check_actions('submit_show_post_extra_fields'); ?>
-			</div>
-				
-			<div class="show_post_extras">
-				<?php $plugin->check_actions('submit_show_post_extras'); ?>
-			</div>
-			
-		</div>
-		
-		<?php $plugin->check_actions('submit_post_show_post'); ?>
-		
-	<?php } ?>
-	
-	<!-- ************ END POST **************** -->
+<!-- POST -->
+<?php if ($post->post_status != 'buried') { ?>
 
+    <?php $result = $plugin->check_actions('submit_pre_show_post'); 
+        if (!isset($result) || !is_array($result)) {
+        // if buried during that plugin call, the post won't show...
+    ?>
+    
+        <div class="show_post vote_button_space_<?php echo $post->post_vars['vote_type'] ?>">
+        
+            <div class="show_post_title"><a href='<?php echo $post->post_orig_url; ?>'><?php echo $post->post_title; ?></a></div>
+        
+            <?php if ($post->use_author || $post->use_date) { ?>
+                <div class="show_post_author_date">    
+                    Posted
+                    <?php if ($post->use_author) { 
+                        echo " by <a href='" . url(array('user' => $userbase->get_username($post->post_author))) . "'>" . $userbase->get_username($post->post_author) . "</a>"; } 
+                    ?>
+                    <?php if ($post->use_date) { echo time_difference(unixtimestamp($post->post_date)) . " ago"; } ?>
+                    <?php $plugin->check_actions('submit_show_post_author_date'); ?>
+                </div>
+            <?php } ?>
+                
+            <?php if ($post->use_content) { ?>
+                <div class="show_post_content"><?php echo $post->post_content; ?></div>
+            <?php } ?>
+            
+            <div class="show_post_extra_fields">
+                <?php $plugin->check_actions('submit_show_post_extra_fields'); ?>
+            </div>
+                
+            <div class="show_post_extras">
+                <?php $plugin->check_actions('submit_show_post_extras'); ?>
+            </div>
+            
+        </div>
+        
+        <?php $plugin->check_actions('submit_post_show_post'); ?>
+        
+    <?php } ?>
+    
 <?php 
 } else {
-	// Show "Post buried" message...
-	$hotaru->message = $lang["vote_alert_post_buried"];
-	$hotaru->message_type = "red";
-	$hotaru->show_message();
+    // Show "Post buried" message...
+    $hotaru->message = $lang["vote_alert_post_buried"];
+    $hotaru->message_type = "red";
+    $hotaru->show_message();
 }
-?> 
-	
- 
- 
+?>
+
+<!-- END POST --> 
--- 0.4/content/plugins/submit/templates/submit_step2.php
+++ 0.4/content/plugins/submit/templates/submit_step2.php
@@ -25,66 +25,66 @@
  
 global $hotaru, $cage, $lang, $post, $plugin, $post_orig_url, $post_orig_title;
 
-if($cage->post->getAlpha('submit2') == 'true') {
-	// Submitted this form...
-	$title_check = $cage->post->noTags('post_title');	
-	$content_check = $cage->post->noTags('post_content');	
-	$post_id = $cage->post->getInt('post_id');	
-	$post->post_id = $post_id;
-	$plugin->check_actions('submit_form_2_assign_from_cage');
-	
-} elseif($cage->post->getAlpha('submit3') == 'edit') {
-	// Come back from step 3 to make changes...
-	$title_check = $post->post_title;
-	$content_check = $post->post_content;
-	$post_orig_url = $post->post_orig_url;
-	$post_id = $post->post_id;
+if ($cage->post->getAlpha('submit2') == 'true') {
+    // Submitted this form...
+    $title_check = $cage->post->noTags('post_title');    
+    $content_check = $cage->post->noTags('post_content');    
+    $post_id = $cage->post->getInt('post_id');    
+    $post->post_id = $post_id;
+    $plugin->check_actions('submit_form_2_assign_from_cage');
+    
+} elseif ($cage->post->getAlpha('submit3') == 'edit') {
+    // Come back from step 3 to make changes...
+    $title_check = $post->post_title;
+    $content_check = $post->post_content;
+    $post_orig_url = $post->post_orig_url;
+    $post_id = $post->post_id;
 } else {
-	// First time here...
-	$title_check = $post_orig_title;
-	$content_check = "";
-	$post_id = 0;
-	$plugin->check_actions('submit_form_2_assign_blank');
+    // First time here...
+    $title_check = $post_orig_title;
+    $content_check = "";
+    $post_id = 0;
+    $plugin->check_actions('submit_form_2_assign_blank');
 }
 
 $plugin->check_actions('submit_form_2_assign');
 
 ?>
 
-	<p class='breadcrumbs'><a href='<?php echo baseurl ?>'><?php echo $lang['submit_form_home'] ?></a> &raquo; <?php echo $lang["submit_form_step2"] ?></p>
-		
-	<?php echo $hotaru->show_messages(); ?>
-			
-	
-	<?php echo $lang["submit_form_instructions_2"] ?>
+    <div id="breadcrumbs"><a href='<?php echo baseurl ?>'><?php echo $lang['submit_form_home'] ?></a> &raquo; <?php echo $lang["submit_form_step2"] ?></div>
+        
+    <?php echo $hotaru->show_messages(); ?>
+            
+    
+    <?php echo $lang["submit_form_instructions_2"] ?>
 
-	<form name='submit_form_2' action='<?php baseurl ?>index.php?page=submit2&sourceurl=<?php echo $post_orig_url ?>' method='post'>
-	<table>
-	<tr>
-		<td><?php echo $lang["submit_form_url"] ?>&nbsp; </td>
-		<td><?php echo $post_orig_url; ?></td>
-		<td>&nbsp;</td>
-	</tr>
-	<tr>
-		<td><?php echo $lang["submit_form_title"] ?>&nbsp; </td>
-		<td><input type='text' size=50 id='post_title' name='post_title' value='<?php echo $title_check ?>'></td>
-		<td id='ajax_loader'>&nbsp;</td>
-	</tr>
-	
-	<?php if($post->use_content) { ?>
-	<tr>
-		<td style='vertical-align: top;'><?php echo $lang["submit_form_content"] ?>&nbsp; </td>
-		<td colspan='2'><textarea id='post_content' name='post_content' rows='6' maxlength='<?php $post->post_content_length; ?>' style='width: 32em;'><?php echo $content_check ?></textarea></td>
-	</tr>
-	<?php } ?>
-	
-	<?php $plugin->check_actions('submit_form_2_fields'); ?>
-		
-	<input type='hidden' name='post_orig_url' value='<?php echo $post_orig_url; ?>' />
-	<input type='hidden' name='post_id' value='<?php echo $post_id ?>' />
-	<input type='hidden' name='submit2' value='true' />
-	
-	<tr><td colspan=3>&nbsp;</td></tr>
-	<tr><td>&nbsp; </td><td>&nbsp; </td><td style='text-align:right;'><input type='submit' onclick="javascript:safeExit=true;" name='submit' value='<?php echo $lang['submit_form_submit_next_button'] ?>' /></td></tr>	
-	</table>
-	</form>
+    <form name='submit_form_2' action='<?php baseurl ?>index.php?page=submit2&sourceurl=<?php echo $post_orig_url ?>' method='post'>
+    <table>
+    <tr>
+        <td><?php echo $lang["submit_form_url"] ?>&nbsp; </td>
+        <td><?php echo $post_orig_url; ?></td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td><?php echo $lang["submit_form_title"] ?>&nbsp; </td>
+        <td><input type='text' size=50 id='post_title' name='post_title' value='<?php echo $title_check ?>'></td>
+        <td id='ajax_loader'>&nbsp;</td>
+    </tr>
+    
+    <?php if ($post->use_content) { ?>
+    <tr>
+        <td style='vertical-align: top;'><?php echo $lang["submit_form_content"] ?>&nbsp; </td>
+        <td colspan='2'><textarea id='post_content' name='post_content' rows='6' maxlength='<?php $post->post_content_length; ?>' style='width: 32em;'><?php echo $content_check ?></textarea></td>
+    </tr>
+    <?php } ?>
+    
+    <?php $plugin->check_actions('submit_form_2_fields'); ?>
+        
+    <input type='hidden' name='post_orig_url' value='<?php echo $post_orig_url; ?>' />
+    <input type='hidden' name='post_id' value='<?php echo $post_id ?>' />
+    <input type='hidden' name='submit2' value='true' />
+    
+    <tr><td colspan=3>&nbsp;</td></tr>
+    <tr><td>&nbsp; </td><td>&nbsp; </td><td style='text-align:right;'><input type='submit' onclick="javascript:safeExit=true;" name='submit' value='<?php echo $lang['submit_form_submit_next_button'] ?>' /></td></tr>    
+    </table>
+    </form>
