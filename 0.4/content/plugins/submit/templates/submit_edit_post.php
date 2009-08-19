3f8f18774ac3881b4c5d1161ca3c1b7cdd0ad743
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-08-19 06:58:43 +0000

    Whoops, forgot the template for post editing...

---
+++ 0.4/content/plugins/submit/templates/submit_edit_post.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Template for Submit: Edit Post
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+ 
+global $hotaru, $cage, $lang, $post, $plugin, $post_orig_url, $post_orig_title, $current_user;
+
+if ($cage->post->getAlpha('edit_post') == 'true') {
+    // Submitted this form...
+    $title_check = $cage->post->noTags('post_title');    
+    $content_check = $cage->post->noTags('post_content');    
+    $post_id = $cage->post->getInt('post_id');    
+    $post->post_id = $post_id;
+    $plugin->check_actions('submit_form_2_assign_from_cage');
+    
+} elseif ($cage->get->testInt('post_id'))  {
+    $post_id = $cage->get->testInt('post_id');
+    $post->read_post($post_id);
+    $title_check = $post->post_title;
+    $content_check = $post->post_content;
+    $post_orig_url = $post->post_orig_url;
+    $post_id = $post->post_id;
+}
+
+$user = new UserBase();
+$user->get_user_basic($post->post_author);
+if ($current_user->role != 'admin' && ($current_user->id != $user->id)) { 
+    $hotaru->message = "You don't have permission to edit this post.";
+    $hotaru->message_type = "red";
+    $hotaru->show_message();
+    return false;
+    die();
+}
+
+$plugin->check_actions('submit_form_2_assign');
+
+?>
+
+    <div id="breadcrumbs"><a href='<?php echo baseurl ?>'><?php echo $lang['submit_form_home'] ?></a> &raquo; <?php echo $lang["submit_edit_post_title"] ?></div>
+        
+    <?php echo $hotaru->show_messages(); ?>
+            
+    
+    <?php echo $lang["submit_edit_post_instructions"] ?>
+
+    <form name='submit_edit_post' action='<?php baseurl ?>index.php?page=edit_post&sourceurl=<?php echo $post_orig_url ?>' method='post'>
+    <table>
+    <tr>
+        <td><?php echo $lang["submit_form_url"] ?>&nbsp; </td>
+        <td><?php echo $post_orig_url; ?></td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td><?php echo $lang["submit_form_title"] ?>&nbsp; </td>
+        <td><input type='text' size=50 id='post_title' name='post_title' value='<?php echo $title_check ?>'></td>
+        <td id='ajax_loader'>&nbsp;</td>
+    </tr>
+    
+    <?php if ($post->use_content) { ?>
+    <tr>
+        <td style='vertical-align: top;'><?php echo $lang["submit_form_content"] ?>&nbsp; </td>
+        <td colspan='2'><textarea id='post_content' name='post_content' rows='6' maxlength='<?php $post->post_content_length; ?>' style='width: 32em;'><?php echo $content_check ?></textarea></td>
+    </tr>
+    <?php } ?>
+    
+    <?php $plugin->check_actions('submit_form_2_fields'); ?>
+        
+    <input type='hidden' name='post_orig_url' value='<?php echo $post_orig_url; ?>' />
+    <input type='hidden' name='post_id' value='<?php echo $post_id ?>' />
+    <input type='hidden' name='edit_post' value='true' />
+    
+    <tr><td colspan=3>&nbsp;</td></tr>
+    <tr><td>&nbsp; </td><td>&nbsp; </td><td style='text-align:right;'><input type='submit' name='submit_edit_post' value='<?php echo $lang["submit_edit_post_save"] ?>' /></td></tr>    
+    </table>
+    </form>
