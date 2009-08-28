46bfaf0bfd684b99b2fe63814a5cbed89621d63b
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-08-28 01:36:10 +0000

    Re-enabled trackbacks, added cache options and extended the maintenance page with options to optimize all tables, and empty or delete plugin-created tables.

--- 0.5/content/admin_themes/admin_default/maintenance.php
+++ 0.5/content/admin_themes/admin_default/maintenance.php
@@ -44,10 +44,15 @@ global $plugin, $admin, $cage, $lang; // don't remove
         if ($action == 'clear_db_cache') { $admin->clear_cache('db_cache'); }
         if ($action == 'clear_css_js_cache') { $admin->clear_cache('css_js_cache'); }
         if ($action == 'clear_rss_cache') { $admin->clear_cache('rss_cache'); }
+        if ($action == 'optimize') { $admin->optimize_tables(); }
+        if ($action == 'empty') { $admin->empty_table($cage->get->testAlnumLines('table')); }
+        if ($action == 'drop') { $admin->drop_table($cage->get->testAlnumLines('table')); }
     }
-    
 ?>
+
+
 <br />
+<h2><?php echo $lang["admin_theme_maintenance_cache"]; ?></h2>
 <ul>
     <li><a href="<?php echo BASEURL; ?>admin/admin_index.php?page=maintenance&amp;action=clear_db_cache">
         <?php echo $lang["admin_theme_maintenance_db_cache"]; ?></a> - <?php echo $lang["admin_theme_maintenance_db_cache_desc"]; ?></li>
@@ -55,7 +60,34 @@ global $plugin, $admin, $cage, $lang; // don't remove
         <?php echo $lang["admin_theme_maintenance_css_js_cache"]; ?></a> - <?php echo $lang["admin_theme_maintenance_css_js_cache_desc"]; ?></li>
     <li><a href="<?php echo BASEURL; ?>admin/admin_index.php?page=maintenance&amp;action=clear_rss_cache">
         <?php echo $lang["admin_theme_maintenance_rss_cache"]; ?></a> - <?php echo $lang["admin_theme_maintenance_rss_cache_desc"]; ?></li>
-</ul>    
+</ul>
+
+<br />
+<h2><?php echo $lang["admin_theme_maintenance_database"]; ?></h2>
+<ul>
+    <li><a href="<?php echo BASEURL; ?>admin/admin_index.php?page=maintenance&amp;action=optimize">
+        <?php echo $lang["admin_theme_maintenance_optimize"]; ?></a> - <?php echo $lang["admin_theme_maintenance_optimize_desc"]; ?></li>
+</ul>
+
+<?php $plugin_tables = $admin->list_plugin_tables(); ?>
+<br />
+<h2><?php echo $lang["admin_theme_maintenance_plugin_tables"]; ?></h2>
+<?php echo $lang["admin_theme_maintenance_plugin_table_explanation"]; ?><br /><br />
+<span style='color: red;'><?php echo $lang["admin_theme_maintenance_plugin_table_warning"]; ?></span><br /><br />
+<?php echo $lang["admin_theme_maintenance_empty_explanation"]; ?><br /><br />
+<ul>
+    <?php foreach ($plugin_tables as $table) { ?>
+    <li><a href="<?php echo BASEURL; ?>admin/admin_index.php?page=maintenance&amp;action=empty&amp;table=<?php echo $table; ?>">
+        <?php echo $lang["admin_theme_maintenance_empty"] . " " . $table; ?> </a></li>
+    <?php } ?>
+</ul>
+<br />
+<?php echo $lang["admin_theme_maintenance_drop_explanation"]; ?><br /><br />
+<ul>
+    <?php foreach ($plugin_tables as $table) { ?>
+    <li><a href="<?php echo BASEURL; ?>admin/admin_index.php?page=maintenance&amp;action=drop&amp;table=<?php echo $table; ?>">
+        <?php echo $lang["admin_theme_maintenance_drop"] . " " . $table; ?> </a></li>
+    <?php } ?>
+</ul>
     
 <?php $plugin->check_actions('admin_maintenance_bottom'); ?>
-
