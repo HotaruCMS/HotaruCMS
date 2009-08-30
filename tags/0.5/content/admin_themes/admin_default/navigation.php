1f8437db9ec88efaf65328b6262c3f72e6293692
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-08-30 03:15:57 +0000

    Tagging Hotaru 0.5

---
+++ tags/0.5/content/admin_themes/admin_default/navigation.php
@@ -0,0 +1,50 @@
+<?php 
+/**
+ * Theme name: admin_default
+ * Template name: navigation.php
+ * Template author: Nick Ramsay
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+
+global $hotaru, $plugin, $current_user, $lang; // don't remove
+?>
+
+<ul id="navigation">
+    <?php $plugin->check_actions('navigation_first'); ?>
+    <li><a href="<?php echo BASEURL; ?>"><?php echo $lang["admin_theme_navigation_home"]; ?></a></li>
+    <?php $plugin->check_actions('navigation'); ?>
+    <?php $plugin->check_actions('admin_navigation'); ?>
+    <?php 
+        if (!$plugin->plugin_active('users')) { 
+
+            if ($current_user->logged_in == true) { 
+                echo "<li><a id='navigation_active' href='" . url(array(), 'admin') . "'>" . $lang["admin_theme_navigation_admin"] . "</a></li>"; 
+                echo "<li><a href='" . url(array('page'=>'admin_logout'), 'admin') . "'>" . $lang["admin_theme_navigation_logout"] . "</a></li>";
+            } else { 
+                echo "<li><a href='" . url(array(), 'admin') . "'>" . $lang["admin_theme_navigation_login"] . "</a></li>"; 
+            }
+        } else {
+            $plugin->check_actions('navigation_users', true, 'users'); // ensures login/logout/register are last.
+        }
+    ?>
+</ul>
---
+++ tags/0.5/index.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Gateway to the rest of Hotaru
+ *
+ * Displays the index template in the chosen theme
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+global $cage;
+
+// includes
+require_once('hotaru_header.php');
+
+// Include "main" language file
+if (file_exists(LANGUAGES . LANGUAGE_PACK . 'main_language.php'))
+{
+    // language file from the chosen language pack
+    include_once(LANGUAGES . LANGUAGE_PACK . 'main_language.php');
+}
+else 
+{
+   // try the default language pack
+    require_once(LANGUAGES . 'language_default/main_language.php'); 
+}
+
+// Include combined css and js files
+if ($cage->get->keyExists('combine')) {
+    $type = $cage->get->testAlpha('type');
+    $version = $cage->get->testInt('version');
+    $hotaru->combine_includes($type, $version);
+}
+
+$hotaru->display_template('index');
+?>
