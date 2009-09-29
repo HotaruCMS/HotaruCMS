a3eb008e7118a5751e7fc3e875e4f3aafe25fb2c
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-09-29 08:27:38 +0000

    Bug fixes for ReCaptcha and email confirmation.

--- content/plugins/users/users_settings.php
+++ content/plugins/users/users_settings.php
@@ -66,8 +66,8 @@ class UsersSettings extends Users
         
         $thisdomain =  rstrtrim(str_replace("http://", "", BASEURL), '/');
         echo "<input type='checkbox' name='rc_enabled' value='enabled' " . $recaptcha_enabled . " >&nbsp;&nbsp;" . $lang["users_settings_recaptcha_enable"] . " <a href='http://recaptcha.net/api/getkey?domain=" . $thisdomain . "&app=HotaruCMS'>reCAPTCHA.net</a><br /><br />\n";    
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $lang["users_settings_recaptcha_public_key"] . ": <input type='text' name='rc_pubkey' value='" . $recaptcha_pubkey . "'><br /><br />\n";
-        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $lang["users_settings_recaptcha_private_key"] . ": <input type='text' name='rc_privkey' value='" . $recaptcha_privkey . "'><br /><br />\n";
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $lang["users_settings_recaptcha_public_key"] . ": <input type='text' name='rc_pubkey' size=50 value='" . $recaptcha_pubkey . "'><br /><br />\n";
+        echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" . $lang["users_settings_recaptcha_private_key"] . ": <input type='text' name='rc_privkey' size=50 value='" . $recaptcha_privkey . "'><br /><br />\n";
         echo "<input type='checkbox' name='emailconf' value='emailconf' " . $emailconf_enabled . ">&nbsp;&nbsp;" . $lang["users_settings_email_conf"] . "<br />\n";
     
         $this->pluginHook('users_settings_form');
@@ -89,31 +89,31 @@ class UsersSettings extends Users
         // Recaptcha Enabled
         if ($cage->post->keyExists('rc_enabled')) { 
             $recaptcha_enabled = 'checked'; 
-            $userbase->vars['use_recaptcha'] = true;
+            $userbase->vars['useRecaptcha'] = true;
         } else { 
             $recaptcha_enabled = ''; 
-            $userbase->vars['use_recaptcha'] = false;
+            $userbase->vars['useRecaptcha'] = false;
         }
         
         // Email Confirmation Enabled
         if ($cage->post->keyExists('emailconf')) { 
             $emailconf_enabled = 'checked'; 
-            $userbase->vars['use_emailconf'] = true;
+            $userbase->vars['useEmailConf'] = true;
         } else { 
             $emailconf_enabled = ''; 
-            $userbase->vars['use_emailconf'] = false;
+            $userbase->vars['useEmailConf'] = false;
         }
         
         // ReCaptcha Public Key
         if ($cage->post->keyExists('rc_pubkey')) { 
-            $recaptcha_pubkey = $cage->post->getAlnum('rc_pubkey');
+            $recaptcha_pubkey = $cage->post->testAlnumLines('rc_pubkey');
         } else { 
             $recaptcha_pubkey = "";
         }
         
         // ReCaptcha Private Key
         if ($cage->post->keyExists('rc_privkey')) {     
-            $recaptcha_privkey = $cage->post->getAlnum('rc_privkey');
+            $recaptcha_privkey = $cage->post->testAlnumLines('rc_privkey');
         } else { 
             $recaptcha_privkey = ""; 
         }
