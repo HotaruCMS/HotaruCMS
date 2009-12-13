b748c8fe9cc370d9f20d8be6fa70ec10869cbf1b
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2009-12-13 23:35:29 +0000

    Breadcrumbs class (forgot to attach to previous commit). (1.0)

---
+++ libs/Breadcrumbs.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Breadcrumb functions
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+class Breadcrumbs
+{
+    /**
+     * Build breadcrumbs
+     */
+    public function buildBreadcrumbs($hotaru)
+    {
+        $output = '';
+        $output .= "<a href='" . BASEURL . "'>" . SITE_NAME . "</a>\n"; 
+        
+        // Admin only:
+        if ($hotaru->isAdmin) {
+            $output .= " &raquo; <a href='" . $hotaru->url(array(), 'admin') . "'>";
+            $output .= $hotaru->lang['admin_theme_main_admin_cp'] . "</a>\n";
+        }
+        
+        // plugin hook:
+        $crumbs = $hotaru->pluginHook('breadcrumbs');
+        if (isset($crumbs)) {
+            $output .= $crumbs['breadcrumbs']; // I KNOW THIS WON'T WORK.
+            return $output;
+        } 
+        
+        // in case of no plugins:
+        $output .= " &raquo; " . $hotaru->pageTitle;
+
+        return $output;
+    }
+}
+?>
