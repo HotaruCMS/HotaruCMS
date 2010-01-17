2c790ad43dd862e306a8637b88173d1b0ce872d1
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-01-17 16:50:24 +0000

    [Branch] Typo fix.

--- content/themes/default/footer.php
+++ content/themes/default/footer.php
@@ -30,7 +30,7 @@
 
     <div id="ft">
         <?php 
-            $h->plugitnHook('footer');
+            $h->pluginHook('footer');
         
             // Link to forums...
             echo "<p><a href='http://hotarucms.org' title='" . $h->lang["main_theme_footer_hotaru_link"] . "'><img src='" . BASEURL . "content/themes/" . THEME . "images/hotarucms.png' ";
