96e227494282be91a3640de77abd1aee767ee0b5
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-01-24 13:40:32 +0000

    [Branches] Changes to Debug so that errors get logged in the cache only when Debug mode is on. At other times, they are logged wherever PHP usually logs them.

--- libs/Initialize.php
+++ libs/Initialize.php
@@ -82,14 +82,6 @@ class Initialize
         // display errors
         ini_set('display_errors', 1); // Gets disabled later in checkDebug()
         error_reporting(E_ALL);
-        
-        // log errors to a file - the custom error handler below wasn't catching fatal errors, so using PHP's one
-        ini_set('error_log', CACHE . 'debug_logs/error_log.txt');
-        /*
-        require_once(EXTENSIONS . 'SWCMS/swcms_error_handler.php'); // error_handler class
-        $error_handler = new swcms_error_handler(0, 0, 1, NULL, CACHE . 'debug_logs/error_log.txt');
-        set_error_handler(array($error_handler, "handler"));
-        */
     }
 
 
@@ -231,7 +223,8 @@ class Initialize
         if (DEBUG == "true") {
             require_once(FUNCTIONS . 'funcs.times.php');
             timer_start();
-            ini_set('display_errors', 1); // show errors 
+            ini_set('display_errors', 1); // show errors
+            ini_set('error_log', CACHE . 'debug_logs/error_log.txt');
             return true;
         } else {
             ini_set('display_errors', 0); // hide errors
