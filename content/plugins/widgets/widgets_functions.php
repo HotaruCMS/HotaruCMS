3db29e831070fb6ed73a8cc06f39e8f5016af43a
shibuya246 <shibuya246@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-01-25 14:45:24 +0000

    [branch] experimental ajax code for widgets plugin

---
+++ content/plugins/widgets/widgets_functions.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * file: plugins/widgets/widgets_functions.php
+ * purpose: Voting functions that are performed behind the scenes with Ajax
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+
+//$json_array = array('result'=>'test_okay');
+//echo json_encode($json_array); exit;
+//echo $_SERVER['DOCUMENT_ROOT'];
+
+require_once('../../../hotaru_settings.php');
+require_once('../../../Hotaru.php');    // Not the cleanest way of getting to the root...
+
+$h = new Hotaru();
+$h->start();
+
+if ($h->cage->post->testAlpha('plugin') == 'widgets' ) {
+	if ($h->cage->post->testAlpha('action') == 'enable') { 
+		$this_widget_function = $h->cage->post->testAlnumLines('widget');
+		$this_widget_name = $h->vars['widgets']->getPluginFromFunction($h, $this_widget_function);
+	 
+		// enable a widget
+		if ($h->isActive($this_widget_name)) {
+			$widgets_settings['widgets'][$this_widget_function]['enabled'] = true;			
+			$json_array = array('enabled'=>'true', 'message'=>'xxxxxxxxx', 'color'=>'green'); 
+		} else {
+			// don't enable it if the plugin is inactive		
+			$widgets_settings['widgets'][$this_widget_function]['enabled'] = false;
+			$json_array = array('enabled'=>'false', 'message'=>'xxxxxxxxx', 'color'=>'red'); 						
+		}						
+	} 	
+	elseif ($h->cage->post->testAlpha('action') == 'disable') {
+		$widgets_settings['widgets'][$this_widget_function]['enabled'] = false;
+		$json_array = array('enabled'=>'false', 'message'=>'xxxxxxxxx', 'color'=>'green'); 					
+	}
+            
+	// Save updated widgets settings
+	$h->updateSetting('widgets_settings', serialize($widgets_settings));
+	// Send back result data
+	echo json_encode($json_array);
+}
+
+?>
