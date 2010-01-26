abc9d0d8abee5bf87b4fda1776375c279c917177
shibuya246 <shibuya246@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-01-26 11:29:04 +0000

    [branch] fix cache check for js libraries

--- libs/Initialize.php
+++ libs/Initialize.php
@@ -251,15 +251,15 @@ class Initialize
     public function setUpJsConstants()
     {
         // Start timer if debugging
-		$global_js_var = "jQuery('document').ready(function($) {BASEURL = '". BASEURL ."'; ADMIN_THEME = '" . ADMIN_THEME . "'; });";	
+		$global_js_var = "jQuery('document').ready(function($) {BASEURL = '". BASEURL ."'; ADMIN_THEME = '" . ADMIN_THEME . "'; THEME = '" . THEME . "';});";	
 		$JsConstantsFile = "css_js_cache/JavascriptConstants.js";
 
-		//if (!file_exists(CACHE . $JsConstantsFile) {
+		if (!file_exists(CACHE . $JsConstantsFile)) {
 			$JsConstantsPath = CACHE . $JsConstantsFile;
 			$JsConstantsfh = fopen($JsConstantsPath, 'w') or die ("Can't open file");	
 			fwrite($JsConstantsfh, $global_js_var);
 			fclose($JsConstantsfh);		
-		//}        
+		}        
         return false;
     }
         
