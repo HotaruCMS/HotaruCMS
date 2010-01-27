6174ba2854c2ff0834c320ee4e377b508617aa54
shibuya246 <shibuya246@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-01-27 19:18:20 +0000

    [Branch] missing files req for '1-Click' theme activation

---
+++ content/admin_themes/admin_default/javascript/admin_default.js
@@ -0,0 +1,65 @@
+/* **************************************************************************************************** 
+ *  File: /content/admin_themes/admin_default/javascript/admin_default.js
+ *  Purpose: JQuery for admin_default
+ *  Notes: 
+ *  License:
+ *
+ *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *
+ *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the 
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of 
+ *   the License, or (at your option) any later version.
+ *
+ *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not, 
+ *   see http://www.gnu.org/licenses/.
+ *   
+ *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
+ *
+ **************************************************************************************************** */
+
+
+jQuery('document').ready(function($) {
+
+	$('.accordion li').has('ul').click(function() {
+		
+		$(this).children("ul").toggle('slow');
+		
+	});
+
+    $('#admin_theme_theme_activate').click(function() {        
+        var theme = $(this).attr("name");       
+        var formdata = 'admin=theme_settings&theme='  + theme;
+		var sendurl = BASEURL + "content/admin_themes/" + ADMIN_THEME + 'theme_settings_functions.php';
+
+        $.ajax(
+			{
+			type: 'post',
+				url: sendurl,
+				data: formdata,
+				beforeSend: function () {
+						$('#admin_theme_theme_activate').html('<img src="' + BASEURL + "content/admin_themes/" + ADMIN_THEME + 'images/ajax-loader.gif' + '"/>&nbsp;Attempting to activate theme.');
+					},
+				error: 	function(XMLHttpRequest, textStatus, errorThrown) {
+						$(this).html('ERROR');
+				},
+				success: function(data, textStatus) { // success means it returned some form of json code to us. may be code with custom error msg
+					if (data.error === true) {
+					}
+					else
+					{										
+                        $('#admin_theme_theme_activate').html(data.message);
+					}
+					$('.message').html(data.message).addClass(data.color, 'visible');
+				},
+				dataType: "json"
+		});
+
+    });
+
+
+	
+});	
