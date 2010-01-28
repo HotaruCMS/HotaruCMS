067f06474a6d0184eed3653c6720d0536fd938da
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-01-28 03:59:26 +0000

    [Branch] HTML edit in Media Select to match the category bar which was updated a while ago.

--- content/plugins/media_select/media_select.php
+++ content/plugins/media_select/media_select.php
@@ -196,8 +196,8 @@ class MediaSelect
      */
     public function category_bar_end($h)
     {
-        echo "<li><a href='#'>" . $h->lang['media_select'] . "\n";
-        echo "<ul>";
+        echo "<li><a href='#'>" . $h->lang['media_select'] . "</a>\n";
+        echo "<ul class='children'>\n";
         echo "<li><a href='" . $h->url(array('media'=>'text')) . "'>" . $h->lang['media_select_text'] . "</a>\n";
         echo "<li><a href='" . $h->url(array('media'=>'video')) . "'>" . $h->lang['media_select_videos'] . "</a>\n";
         echo "<li><a href='" . $h->url(array('media'=>'image')) . "'>" . $h->lang['media_select_images'] . "</a>\n";
