602e1a4d636572b2e9b645ce278f897cc360e892
shibuya246 <shibuya246@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-01-31 12:16:40 +0000

    [Branch] add jquery function to rollover on table for user manager

---
+++ content/plugins/user_manager/javascript/user_manager.js
@@ -0,0 +1,34 @@
+/* ****************************************************************************************************
+ *  File: /plugins/user_manager/javascript/user_manager.js
+ *  Purpose: Ajax for User_Manager Plugin
+ *  Notes:
+ *  License:
+ *
+ *   This file is part of Hotaru CMS (http://www.hotarucms.org/).
+ *
+ *   Hotaru CMS is free software: you can redistribute it and/or modify it under the terms of the
+ *   GNU General Public License as published by the Free Software Foundation, either version 3 of
+ *   the License, or (at your option) any later version.
+ *
+ *   Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ *   even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along with Hotaru CMS. If not,
+ *   see http://www.gnu.org/licenses/.
+ *
+ *   Copyright (C) 2009 Hotaru CMS - http://www.hotarucms.org/
+ *
+ **************************************************************************************************** */
+
+jQuery('document').ready(function($) {
+
+    $("#post_man_table tr").hover(function(){       
+        $this = $(this);       
+        var td =$(this).children('.pm_author');        
+        var icons = td.children('.user_manager_name_icons');        
+        icons.toggle();        
+    });
+
+});
+
