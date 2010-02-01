f44fdfbfaa1c0ab4c29f6f6351e058899d6faee7
rushnp774 <rushnp774@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-02-01 04:02:13 +0000

    [branch] Tweet This module with the ability to add Google Analytics tracking tags to shortened URLs

---
+++ content/plugins/tweet this with tracking/languages/tweet_this_language.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * RPX LANGUAGE
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+
+/* Tweet This link */
+$lang['tweet_this'] = "Tweet This!";
+
+/* Admin options */
+$lang['tweet_this_settings_header'] = "Tweet This Settings";
+$lang['tweet_this_settings_shortener'] = "Choose the url shortener you want to use. If you choose bit.ly, you must provide your bit.ly login and API key.";
+$lang['tweet_this_settings_tinyurl'] = "tinyurl";
+$lang['tweet_this_settings_isgd'] = "is.gd";
+$lang['tweet_this_settings_bitly'] = "bit.ly";
+$lang['tweet_this_settings_bitly_login'] = "bit.ly login";
+$lang['tweet_this_settings_bitly_api_key'] = "bit.ly API key";
+$lang['tweet_this_settings_error'] = "You must enter your bitly login and API key to use bitly";
+$lang['tweet_this_settings_use_GA_tracking'] = "Append Google Analytics tracking tags to shortened links?";
+$lang['tweet_this_settings_what_is_GA_tracking'] = "What is this?";
+
+?>
