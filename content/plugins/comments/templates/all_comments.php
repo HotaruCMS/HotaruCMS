e431506678c3d197812ebf3ebe950ee6d76de005
shibuya246 <shibuya246@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-02-02 04:59:18 +0000

    [Branch] change styling for show all comments page to match main comments page

--- content/plugins/comments/templates/all_comments.php
+++ content/plugins/comments/templates/all_comments.php
@@ -25,59 +25,60 @@
  */
  
 
+$h->readPost($h->comment->postId);
+
 ?>
     <a id="c<?php echo $h->comment->id; ?>"></a>
-    
-    <div class="comment">
 
-        <?php   // Show avatars if enabled (requires an avatars plugin)
-                if ($h->comment->avatars == 'checked') {
-                    if($h->isActive('avatar')) {
-                        $h->setAvatar($h->comment->author, $h->comment->avatarSize);
-                        echo $h->wrapAvatar();
-                    }
-                }
-        ?>
-        
+    <?php if ($h->comment->avatarSize < 16) {$comment_header_size=16;} else { $comment_header_size= $h->comment->avatarSize; } ?>
+    <div class="comment" style="margin-left: <?php echo $h->comment->depth * 2.0; ?>em;">
+        <div class="comment_header" style="height:<?php echo $comment_header_size; ?>px;">
+            <div class="comment_header_left">
+                <?php   // Show avatars if enabled (requires an avatars plugin)
+                        if ($h->comment->avatars == 'checked') {
+                            if($h->isActive('avatar')) {
+                                $h->setAvatar($h->comment->author, $h->comment->avatarSize);
+                                echo $h->wrapAvatar();
+                            }
+                        }
+                ?>
+                <div class="comment_author">
+                <?php
+                        $username = $h->getUserNameFromId($h->comment->author);
+                        echo $h->lang['comments_written_by'] . " ";
+                        echo "<a href='" . $h->url(array('user' => $username)) . "'>" . $username . "</a>, ";
+                        echo time_difference(unixtimestamp($h->comment->date), $h->lang) . " ";
+                        echo $h->lang['comments_time_ago'] . ".";
+                ?>
+                </div>
+            </div>
+
         <?php   // Show votes if enabled (requires a comment voting plugin)
                 if ($h->comment->voting == 'checked') {
-                    $h->pluginHook('show_comments_votes'); 
+                    $h->pluginHook('show_comments_votes');
                 }
         ?>
-        
-        <div class="comment_content">
-            <?php 
-                $result = $h->pluginHook('show_comments_content'); 
-                if (!isset($result) || !is_array($result)) {   
-                    echo nl2br($h->comment->content);
-                }
-            ?> 
         </div>
-        
-        <div class="comment_author_date">
-            <?php 
-                $username = $h->getUserNameFromId($h->comment->author);
-                echo $h->lang['comments_written_by'] . " ";
-                echo "<a href='" . $h->url(array('user' => $username)) . "'>" . $username . "</a>, ";
-                echo time_difference(unixtimestamp($h->comment->date), $h->lang) . " " . $h->lang['comments_time_ago'];
-                echo $h->lang['comments_posted_on'] . "<a href='" . $h->url(array('page'=>$h->post->id)) . "#c" . $h->comment->id . "'>" . $h->post->title . "</a>";
-            ?>
-            
-            <?php   // EDIT LINK - (if comment owner AND permission to edit own comments) OR (permission to edit ALL comments)...
-                if (($h->currentUser->id == $h->comment->author && ($h->currentUser->getPermission('can_edit_comments') == 'own'))
-                    || ($h->currentUser->getPermission('can_edit_comments') == 'yes')) { ?>
-                    <a href='#' class='comment_edit_link' onclick="edit_comment(
-                        '<?php echo BASEURL; ?>', 
-                        '<?php echo $h->comment->id; ?>', 
-                        '<?php echo urlencode($h->comment->content); ?>', 
-                        '<?php echo $h->lang['comments_form_edit']; ?>'); 
-                        return false;" ><?php echo $h->lang['comments_edit_link']; ?></a>
-            <?php } ?>
-        </div>
-        
+
         <div class="clear"></div>
-            
+
+        <div class="comment_main">
+            <div class="comment_content">
+                <?php
+               
+                    $result = $h->pluginHook('show_comments_content');
+                    if (!isset($result) || !is_array($result)) {
+                        echo nl2br($h->comment->content);
+                        echo '<div class="comment_post_link">';
+                        echo '<a href="' . $h->url(array('page'=>$h->post->id)) . '">';
+                        echo $h->post->title;
+                        echo '</a>';
+                        echo '</div>';
+                    }
+                ?>
+            </div>
+        </div>
+
     </div>
-    
+
     <div class="clear"></div>
-    
