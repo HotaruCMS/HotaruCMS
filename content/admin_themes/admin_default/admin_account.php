b7ba2f7c94007aa30e5730b051225f33e1e15f12
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-02-23 17:37:49 +0000

    [Branch 1.2] Added password and username instructions to the admin account form.

--- content/admin_themes/admin_default/admin_account.php
+++ content/admin_themes/admin_default/admin_account.php
@@ -36,6 +36,7 @@ extract($h->vars['admin_account']); // extracts $username_check, etc.
     <form name='admin_theme_update_form' action='<?php echo BASEURL; ?>admin_index.php' method='post'>    
     <table>
     <tr><td><?php echo $h->lang["main_user_theme_update_username"]; ?>&nbsp; </td><td><input type='text' size=30 name='username' value='<?php echo $username_check; ?>' /></td></tr>
+    <tr><td colspan='2'><small><?php echo $h->lang["main_user_account_username_requirements"]; ?></small></td></tr>
     <tr><td><?php echo $h->lang["main_user_theme_update_email"]; ?>&nbsp; </td><td><input type='text' size=30 name='email' value='<?php echo $email_check; ?>' /></td></tr>
     <input type='hidden' name='update_type' value='update_general' />
     <input type='hidden' name='page' value='admin_account'>
@@ -52,6 +53,7 @@ extract($h->vars['admin_account']); // extracts $username_check, etc.
     <?php echo $h->lang["main_user_theme_update_password_instruct"]; ?>
     <form name='update_form' action='<?php echo BASEURL; ?>admin_index.php' method='post'>
     <table>
+    <tr><td colspan='2'><small><?php echo $h->lang["main_user_account_password_requirements"]; ?></small></td></tr>
     <tr><td><?php echo $h->lang["main_user_theme_update_old_password"]; ?>&nbsp; </td><td><input type='password' size=30 name='password_old' value='<?php echo $password_check_old; ?>' /></td></tr>
     <tr><td><?php echo $h->lang["main_user_theme_update_new_password"]; ?>&nbsp; </td><td><input type='password' size=30 name='password_new' value='<?php echo $password_check_new; ?>' /></td></tr>
     <tr><td><?php echo $h->lang["main_user_theme_update_new_password_verify"]; ?>&nbsp; </td><td><input type='password' size=30 name='password_new2' value='<?php echo $password_check_new2; ?>' /></td></tr>
