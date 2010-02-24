e83e439f9ac54d195d46b2c3ef28b24cd6f8d81e
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-02-24 06:42:25 +0000

    [Trunk] Jon H's Twitter Widget 0.1

---
+++ content/plugins/twitter_widget/languages/twitter_widget_language.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * TWITTER WIDGET LANGUAGE
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+
+/* Admin Settings */
+$lang['twitter_widget_admin_sidebar'] = "Twitter Widget";
+$lang["twitter_widget_settings_header"] = "Twitter Widget Settings";
+$lang["twitter_widget_settings_instructions"] = "To use this plugin, you must have a <a href='http://www.twitter.com'>Twitter account</a>.";
+$lang["twitter_widget_settings_username"] = "Please enter your Twitter username";
+$lang["twitter_widget_settings_password"] = "Enter your Twitter Password:";
+$lang["twitter_widget_settings_mo_username"] = "Username not entered.";
+$lang["twitter_widget_settings_no_password"] = "Password not entered.";
+$lang["twitter_widget_main_settings_saved"] = "Username and Password Saved.";
+
+/* Sidebar */
+$lang["twitter_widget_follow_us"] = "Follow Us on Twitter!";
+$lang["twitter_widget_followers"] = "Followers";
+
+?>
