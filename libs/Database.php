5b3fc02a80ec6910924e0175823526d94e3f171d
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-04-08 07:33:11 +0000

    [Branch 1.2] Forgot to add the new Database class.

---
+++ libs/Database.php
@@ -0,0 +1,182 @@
+<?php
+/**
+ * Database Class
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    Nick Ramsay <admin@hotarucms.org>
+ * @copyright Copyright (c) 2009, Hotaru CMS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link      http://www.hotarucms.org/
+ */
+class Database extends ezSQL_mysql
+{
+	protected $select        = array();
+	protected $table         = '';
+	protected $where         = array();
+	protected $orderby       = '';
+	protected $limit         = '';
+	protected $prepare_array = array();
+	
+	/**
+	 * Access modifier to set protected properties
+	 */
+	public function __set($var, $val)
+	{
+		$this->$var = $val;
+	}
+    
+    
+	/**
+	 * Access modifier to get protected properties
+	 * The & is necessary (http://bugs.php.net/bug.php?id=39449)
+	 */
+	public function &__get($var)
+	{
+		return $this->$var;
+	}
+	
+	
+	/**
+	 * Build an SQL "SELECT" query
+	 *
+	 * @param array $select - associative array of select terms
+	 * @param string $table - full table name including prefix
+	 * @param array $where - associative array of where terms, e.g. array('id = %d' = 5, 'name = %s' = 'tony')
+	 * @param string $orderby - e.g. "post_date DESC"
+	 * @param string $limit - e.g. "10" or "5, 10"
+	 * @return array|false
+	 */
+	public function fillObject($select = array(), $table = '', $where = array(), $orderby = '', $limit = '')
+	{
+		if ($select)  { $this->select = $select; }
+		if ($table)   { $this->table = $table; }
+		if ($where)   { $this->where = $where; }
+		if ($orderby) { $this->orderby = $orderby; }
+		if ($limit)   { $this->limit = $limit; }
+	}
+	
+	
+	/**
+	 * Build an SQL "SELECT" query
+	 *
+	 * @param array $select - associative array of select terms
+	 * @param string $table - abbreviated table name, e.g. posts, users, comments
+	 * @param array $where - associative array of where terms, e.g. array('id = %d' = 5, 'name = %s' = 'tony')
+	 * @param string $orderby - e.g. post_date DESC
+	 * @param array $limit - X, Y array
+	 * @return array|false
+	 */
+	public function select($h, $select = array(), $table = '', $where = array(), $orderby = '', $limit = array())
+	{
+		// for flexibility, we want to use object properties:
+		$this->fillObject($select, $table, $where, $orderby, $limit);
+		
+		$this->prepare_array = array();
+		$this->prepare_array[0] = "temp";    // placeholder to be later filled with the SQL query.
+		
+		// set SELECT:
+		$select = ($this->select) ? implode(', ', $this->select) : '';
+		
+		// set TABLE:
+		$table = ($this->table) ? DB_PREFIX . $this->table : TABLE_POSTS; // defaults to TABLE_POSTS
+		
+		// set WHERE
+		$where = ($this->where) ? $this->buildWhere() : '';
+		
+		// set ORDER BY
+		$orderby = ($this->orderby) ? ' ORDER BY ' . $this->orderby : '';
+		
+		// set LIMIT
+		$limit = ($this->limit) ? ' LIMIT ' . $this->limit : '';
+		
+		// Build query:
+		$sql = "SELECT " . $select . " FROM " . $table . $where . $orderby . $limit;
+		
+		$this->prepare_array[0] = $sql; 
+		
+		/*	Example:
+			$this->prepare_array[0] is "SELECT user_id FROM hotaru_users WHERE user_id = %d"
+			$this->prepare_array[1] is "5", where 5 fills the %d
+		*/
+		
+		// get the data and return it
+		return $this->getData($h, $this->table);
+	}
+	
+	
+	/**
+	 * Build the WHERE string
+	 *
+	 * @return string
+	 */
+	public function buildWhere()
+	{
+		if (!$this->where) { return ''; }
+		
+		$filter = " WHERE ";
+		foreach ($this->where as $key => $value) {
+			$filter .= $key . " AND ";    // e.g. " post_tags LIKE %s "
+			
+			// Push the values of %s and %d into the prepare_array
+			
+			// sometimes the filter might contain multiple values, eg.
+			// WHERE post_status = %s OR post_status = %s. In that case,
+			// the values are stored in an array, e.g. array('top', 'new').
+			if (is_array($value)) {
+				foreach ($value as $v) {
+					array_push($this->prepare_array, $v);
+				}
+			} else {
+				// otherwise, push the single value into $this->prepare_array:
+				array_push($this->prepare_array, $value);
+			}
+	
+		}
+		$filter = rstrtrim($filter, " AND "); // strip off trailing AND
+		
+		return $filter;
+	}
+	
+	
+	/**
+	 * Gets data from the database
+	 *
+	 * @param string $table - table name without a prefix.
+	 * @param array $prepare array - obtained from select(), etc.
+	 * @return array|false - object array of data
+	 */
+	public function getData($h, $table = 'posts')
+	{
+		if (!$this->prepare_array) { return false; }
+		
+		if (empty($this->prepare_array[1])) {
+			// there aren't any %d or %s parameters to fill in, so we'll skip the prepare function
+			$h->smartCache('on', $table, 60, $this->prepare_array[0]); // start using cache
+			$data = $h->db->get_results($this->prepare_array[0]); // ignoring the prepare function.
+		} else {
+			$query = $h->db->prepare($this->prepare_array);
+			$h->smartCache('on', $table, 60, $query); // start using cache
+			$data = $h->db->get_results($query);
+		}
+		
+		$h->smartCache('off'); // stop using cache
+		
+		if ($data) { return $data; } else { return false; }
+	}
+}
+?>
