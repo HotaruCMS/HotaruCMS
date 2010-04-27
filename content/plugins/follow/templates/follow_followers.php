012a7f1886b5e96bb117a9e43003c6c59b4d3cca
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-04-27 14:12:38 +0000

    [Branch 1.2] Follow: Fix for missing getLatestActivity function when used with the most recent version of the Activity plugin for Hotaru 1.2.

--- content/plugins/follow/templates/follow_followers.php
+++ content/plugins/follow/templates/follow_followers.php
@@ -62,7 +62,8 @@
                 <td class="follow_activity">
 		    <?php if($h->isActive('activity')) {			
 			     $Activity = new Activity();			     
-			     $LatestActivity = $Activity->getLatestActivity($h, 1, $user->user_id);
+			     // $LatestActivity = $Activity->getLatestActivity($h, 1, $user->user_id);
+			     $LatestActivity = $h->getLatestActivity(1, $user->user_id);
 			     $action = $LatestActivity[0];			     
 			     if (!$action) { echo "No activity yet."; } else {
 				echo $Activity->activityContent($h, $action);
