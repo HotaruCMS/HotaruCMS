15743a5f78ceb942e5e696697eff0d145d85511b
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-04-30 15:58:10 +0000

    [Branch 1.2] Pagination fix for Follow.

--- content/plugins/follow/follow.php
+++ content/plugins/follow/follow.php
@@ -129,15 +129,15 @@ class Follow
 		    $result = $h->follow($h->vars['user']->id);
 		    $h->messages[$h->lang['follow_newfollow']] = 'green';
 		    $query = $h->getFollowers($h->vars['user']->id, 'query');
-		    $h->vars['follow_list'] = $h->pagination($query, count($query), 20);
 		    $h->vars['follow_count'] = $h->countFollowers($h->vars['user']->id);
+		    $h->vars['follow_list'] = $h->pagination($query, $h->vars['follow_count'], 20);
 		    break;
 		case 'unfollow':
 		    $result = $h->unfollow($h->vars['user']->id);
 		    $h->messages[$h->lang['follow_unfollow']] = 'green';
 		    $query = $h->getFollowers($h->vars['user']->id, 'query');
-		    $h->vars['follow_list'] = $h->pagination($query, count($query), 20);
 		    $h->vars['follow_count'] = $h->countFollowers($h->vars['user']->id);
+		    $h->vars['follow_list'] = $h->pagination($query, $h->vars['follow_count'], 20);
 		    break;
 		}
 	}
