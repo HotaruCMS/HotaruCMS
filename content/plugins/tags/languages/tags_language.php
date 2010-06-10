70e5410ef1a737fe42d29926b7dc5d62650e2572
shibuya246 <shibuya246@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-06-10 12:02:59 +0000

    [Extensions] Added admin settings page, exclusion words for tags for showing on post,lists page

--- content/plugins/tags/languages/tags_language.php
+++ content/plugins/tags/languages/tags_language.php
@@ -10,5 +10,10 @@ $lang['tags_meta_description_after'] = '';
 $lang['tags_meta_keywords_more'] = ''; // e.g. ", sport, exercise" - you need the first comma.
 
 /* RSS */
-$lang['post_rss_tagged'] = "Stories tagged"; 
+$lang['post_rss_tagged'] = "Stories tagged";
+
+
+$lang['tags_settings_header'] = "Tags Settings";
+$lang['tags_setting_exclude_words'] = "Tag words to exclude (separate by comma)";
+$lang['tags_setting_exclude_active'] = "Exclusion words active";
 ?>
--- content/plugins/tags/readme.txt
+++ content/plugins/tags/readme.txt
@@ -12,6 +12,7 @@ Instructions
 
 Changelog
 ---------
+v.1.8 2010/06/10 - Alan - Added admin settings page, exclusion tag words for list and post page
 v.1.7 2010/05/22 - Nick - Fix for home page title tags and moved Tag RSS from SB Base into this plugin
 v.1.6 2010/04/14 - Nick - Top stories page name changed from "index" to "popular"
 v.1.5 2010/02/20 - Nick - Meta description and keywords added for browsing tag pages
