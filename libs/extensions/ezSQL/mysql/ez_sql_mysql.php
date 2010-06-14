bbd2604795b84bc86e5a813053704995a491a6b0
shibuya246 <shibuya246@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-06-14 14:56:46 +0000

    [Branch 1.4] sql for multiste

--- libs/extensions/ezSQL/mysql/ez_sql_mysql.php
+++ libs/extensions/ezSQL/mysql/ez_sql_mysql.php
@@ -168,7 +168,7 @@
         *  Perform mySQL query and try to detirmin result value
         */
 
-        function query($query = '')
+        function query($query = '', $add_siteid_to_query = TRUE)
         {
 
             // Initialise return
@@ -215,7 +215,7 @@
                 $this->selectDB($this->dbname);
             }
 
-	    if (MULTI_SITE == 'true') { $query = $this->whereMultiSite($query); }
+	    if (MULTI_SITE == 'true' && $add_siteid_to_query) { $query = $this->whereMultiSite($query); }
 
             // Perform the query via std mysql_query function..
             $this->result = @mysql_query($query,$this->dbh);
@@ -390,30 +390,33 @@
 
 	    if (stripos($query, ' FROM ')  !== false) {
 		$array = explode('FROM ',$query);
-		
-		if (!isset($array[1])) { var_dump($array);  }
-		$array2 = explode(' ', $array[1]);
-		$table = $array2[0];
 
-		$array3 = explode('_', $table);
-		$tablename = $array3[1];
+		if ($array[0] != 'SHOW COLUMNS') {
 
-		$tablename = str_replace(',', '', $tablename);
+		    if (!isset($array[1])) { var_dump($array);  }
+		    $array2 = explode(' ', $array[1]);
+		    $table = $array2[0];
 
-		if (array_key_exists($tablename, $siteidtables)) {
-		    if (stripos($query, $table)) { 
-			if (stripos($query, 'WHERE') !== false) {
-			    $array = explode('WHERE ', $query);
-			    $query = $array[0] . ' WHERE ' . $siteidtables[$tablename] . '_siteid = ' . SITEID . " AND " . $array[1];
-			} else {	
-			    $array = $array = explode('FROM ' . $table ,$query);
-			    $query = $array[0] . ' FROM ' . $table . ' WHERE ' . $siteidtables[$tablename] . '_siteid = ' . SITEID . $array[1];
+		    $array3 = explode('_', $table);
+		    $tablename = $array3[1];
+
+		    $tablename = str_replace(',', '', $tablename);
+
+		    if (array_key_exists($tablename, $siteidtables)) {
+			if (stripos($query, $table)) {
+			    if (stripos($query, 'WHERE') !== false) {
+				$array = explode('WHERE ', $query);
+				$query = $array[0] . ' WHERE ' . $siteidtables[$tablename] . '_siteid = ' . SITEID . " AND " . $array[1];
+			    } else {
+				$array = $array = explode('FROM ' . $table ,$query);
+				$query = $array[0] . ' FROM ' . $table . ' WHERE ' . $siteidtables[$tablename] . '_siteid = ' . SITEID . $array[1];
+			    }
+
+			    $after =  "<span style='color:red; font-weight:bold;'>AFTER</span>: " . $query . "<br/><br/>";
+    //print $after;
 			}
-
-			$after =  "<span style='color:red; font-weight:bold;'>AFTER</span>: " . $query . "<br/><br/>";
-//print $after;
 		    }
-		}		
+		}
 	    } 
 
 
@@ -438,12 +441,16 @@
 	    }
 	    
 	    if (stripos($query, 'INSERT INTO ')  !== false) {
-  //  print "<br/>this is INSERT " . $before;
-		$pattern = '/^INSERT INTO(.*?)\(/';
+   // print "<br/>this is INSERT " . $before;
+		$pattern = '/^INSERT INTO(.*?)\SET/';
 		preg_match($pattern, $query, $matches);
-		
-		if ($matches) { $tablename = trim($matches[1]);	}
-		//print $tablename;
+		if ($matches) { $tablename = trim($matches[1]);	} else {
+		    $pattern = '/^INSERT INTO(.*?)\(/';
+		    preg_match($pattern, $query, $matches);
+		    if ($matches) { $tablename = trim($matches[1]);	}
+		}
+
+//		print $tablename;
 		$tablename = str_ireplace(DB_PREFIX, '', $tablename);
 
 //print ">>>>>>>>>>>>>>" . $tablename . '<<<<<<<<<<<<<<<<<<br/>';
@@ -521,7 +528,7 @@
 
 
 	    //if ($after == 'no') { print $before; }
-//	    print $after;
+	    //print $after;
 	    return $query;
 	}
     }
