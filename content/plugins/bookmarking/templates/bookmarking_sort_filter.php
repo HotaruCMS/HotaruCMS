460b0a35371a9578b251c86243cb43d612182588
nick_ramsay <nick_ramsay@2b46f844-1d64-4159-8e26-b0a61c8db15a>
2010-07-14 11:58:02 +0000

    [Branch 1.4] Removed space within LI tags because they generate unwanted padding.

--- content/plugins/bookmarking/templates/bookmarking_sort_filter.php
+++ content/plugins/bookmarking/templates/bookmarking_sort_filter.php
@@ -34,23 +34,19 @@ if ($h->pageType == 'user') { return false; } // don't show filter on profile, a
     
         <li <?php echo $h->vars['popular_active']; ?>>
             <a href="<?php echo $h->vars['popular_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_recently_popular"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_recently_popular"]; ?></a></li>
 
         <li <?php echo $h->vars['upcoming_active']; ?>>
             <a href="<?php echo $h->vars['upcoming_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_upcoming"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_upcoming"]; ?></a></li>
         
         <li <?php echo $h->vars['latest_active']; ?>>
             <a href="<?php echo $h->vars['latest_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_latest"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_latest"]; ?></a></li>
         
         <li <?php echo $h->vars['all_active']; ?>>
             <a href="<?php echo $h->vars['all_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_all"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_all"]; ?></a></li>
         
         
         <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -58,35 +54,29 @@ if ($h->pageType == 'user') { return false; } // don't show filter on profile, a
         
         <li <?php echo $h->vars['top_24_hours_active']; ?>>
             <a href="<?php echo $h->vars['24_hours_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_top_1_day"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_top_1_day"]; ?></a></li>
         
         <!-- Doesn't fit in the default theme
         <li <?php echo $h->vars['top_48_hours_active']; ?>>
             <a href="<?php echo $h->vars['48_hours_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_top_2_days"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_top_2_days"]; ?></a></li>
         -->
         
         <li <?php echo $h->vars['top_7_days_active']; ?>>
             <a href="<?php echo $h->vars['7_days_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_top_7_days"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_top_7_days"]; ?></a></li>
         
         <li <?php echo $h->vars['top_30_days_active']; ?>>
             <a href="<?php echo $h->vars['30_days_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_top_30_days"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_top_30_days"]; ?></a></li>
         
         <li <?php echo $h->vars['top_365_days_active']; ?>>
             <a href="<?php echo $h->vars['365_days_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_top_365_days"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_top_365_days"]; ?></a></li>
         
         <li <?php echo $h->vars['top_all_time_active']; ?>>
             <a href="<?php echo $h->vars['all_time_link']; ?>">
-            <?php echo $h->lang["bookmarking_sort_top_all_time"]; ?></a>
-        </li>
+            <?php echo $h->lang["bookmarking_sort_top_all_time"]; ?></a></li>
         
     </ul>
 </div>
