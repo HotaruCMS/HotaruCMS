560a33b6929dc95741ed1e5cdd1e1311a48a27e4
Alan <blog@shibuya246.com>
2013-04-20 01:53:05 +0900

    admin language
    minor updates to language file for english version

--- content/admin_language.php
+++ content/admin_language.php
@@ -229,9 +229,9 @@ $lang['pagination_next'] = "Next";
 /* header */
 $lang["admin_theme_header_admin"] = "ADMIN";
 $lang["admin_theme_header_hotarucms"] = "Hotaru CMS ";
-$lang["admin_theme_menu_admin_home"] = "Admin Home";
-$lang["admin_theme_menu_site_home"] = "Site Home";
-$lang["admin_theme_menu_hotaru_forums"] = "Hotaru Forums";
+$lang["admin_theme_menu_admin_home"] = "Admin";
+$lang["admin_theme_menu_site_home"] = "Site";
+$lang["admin_theme_menu_hotaru_forums"] = "Forums";
 $lang["admin_theme_menu_help"] = "Documentation";
 $lang["admin_theme_menu_logout"] = "Logout";
 
