1abb3118114ef136dbc1fc950fe38749ff41029a
Alan <blog@shibuya246.com>
2013-04-21 21:26:24 +0900

    update model

--- models/Users.php
+++ models/Users.php
@@ -2,6 +2,11 @@
 
 class Users extends ActiveRecord\Model
 {
+    # explicit table name since our table is not "users" 
+    static $table_name = 'hotaru_users';    // DB_PREFIX . '_users';
+  
+    # explicit pk since our pk is not "id" 
+    static $primary_key = 'user_id';
 }
 
 ?>
