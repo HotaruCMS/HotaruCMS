208927af13918ddc187a2eaa68716850c311a35b
Alan <blog@shibuya246.com>
2013-05-13 22:41:04 +0900

    char set moved to http-header

--- content/themes/default/header.php
+++ content/themes/default/header.php
@@ -27,11 +27,11 @@
  */
 
 ?>
-
+<?php header('Content-type: text/html; charset=utf-8'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
 <head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="chrome=1" />
 <meta name="generator" content="Hotaru CMS <?php echo $h->version; ?>" />
 
 	<title><?php echo $h->getTitle(); ?></title>
