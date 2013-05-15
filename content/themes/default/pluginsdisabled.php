fc072f68126705e7ba1acf148aad5acc6a09bd53
Alan <blog@shibuya246.com>
2013-05-16 02:50:51 +0900

    temp holder

---
+++ content/themes/default/pluginsdisabled.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
+<div style="padding:15px 25px;" class="hero-unit">
+        <h2>Welcome to Hotaru CMS</h2>
+<!--        <p>It looks like you are just getting started with your new Hotaru CMS website. Why not submit your first post and publish it to the homepage straight away.</p>
+        <p><a href="/submit/" class="btn btn-primary">Submit Your First Post</a></p>-->
+</div>
