fc947864f4211df9250b8bee5c2096167d06a2c7
Alan <blog@shibuya246.com>
2013-05-21 16:10:20 +0900

    nav for admin login lang updated

--- content/themes/default/navigation.php
+++ content/themes/default/navigation.php
@@ -81,7 +81,7 @@
 				echo "<li " . $status . "><a href='" . $h->url(array('page'=>'admin_logout'), 'admin') . "'>" . $h->lang("main_theme_navigation_logout") . "</a></li>";
 			} else { 
 				if ($h->pageName == 'login') { $status = "class='active'"; } else { $status = ""; }
-				echo "<li class='hidden-desktop' " . $status . "><a href='" . $h->url(array(), 'admin') . "'>" . $h->lang("main_theme_navigation_login") . "</a></li>";
+				echo "<li class='hidden-desktop' " . $status . "><a href='" . $h->url(array(), 'admin') . "'>" . $h->lang("main_theme_navigation_admin") . "</a></li>";
                                 
                                 ?>
                                 
