ec748b7059d72188228eaf48eb88468c56cee474
Alan <blog@shibuya246.com>
2013-05-29 20:11:47 +0900

    cant decide whether to move hook up in default theme for header_end

--- content/themes/default/index.php
+++ content/themes/default/index.php
@@ -71,14 +71,14 @@ $sideBarWidth = 12 - $leftSpan;
 			<?php $h->pluginHook('announcement_last'); ?>
 		</div>
 	<?php } ?>
-		
-        <br/>
+		<?php $h->pluginHook('header_end'); ?>
+        
 	<div class="container<?php echo $fluid; ?>">
             <div class="row<?php echo $fluid; ?>">
 
                 <div id="header_end" class="container<?php echo $fluid; ?>">
                         <!-- CATEGORIES, ETC -->
-                        <?php $h->pluginHook('header_end'); ?>
+                        
                 </div>
 
 		<div id="content">
