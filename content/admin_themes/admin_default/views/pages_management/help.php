cd63621e28415b24a5faddf84b746bfbdb9e35c0
Alan <blog@shibuya246.com>
2013-06-03 00:05:37 +0900

    pages

---
+++ content/admin_themes/admin_default/views/pages_management/help.php
@@ -0,0 +1,13 @@
+    <h4>Static page</h4>
+
+    <p>You are able to create and display static pages by saving them in to your /content/pages folder.</p>
+
+    <p>The static pages are independent of the theme, so if you select another theme they will still display albeit with the css styling of your new theme.</p>
+
+    <p>Static pages which are useful to have on your site may be</p>
+
+    <ul>
+        <li>Welcome</li>
+        <li>FAQ</li>
+        <li>Terms of Service</li>        
+    </ul>
