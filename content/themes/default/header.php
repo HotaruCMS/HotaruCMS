038f23bfabecbc149075c9d0d32bdd8a7becb0e8
Alan <blog@shibuya246.com>
2013-06-09 20:49:55 +0900

    theme header settings

--- content/themes/default/header.php
+++ content/themes/default/header.php
@@ -27,7 +27,12 @@
  */
 
 ?>
-<?php header('Content-type: text/html; charset=utf-8'); ?>
+<?php
+header('Content-type: text/html; charset=utf-8');
+//header("Cache-Control: private, max-age=6000, pre-check=6000");
+//header("Pragma: private");
+//header("Expires: " . gmdate("D, d M Y H:i:s"). " GMT");
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
 <head profile="http://gmpg.org/xfn/11">
