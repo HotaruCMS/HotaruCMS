d47c4f1be9ecd4abba499b3c49e1a6aa48e3ff98
Alan <blog@shibuya246.com>
2013-06-12 13:49:01 +0900

    pages listed

--- content/admin_themes/admin_default/views/pages_management/pages.php
+++ content/admin_themes/admin_default/views/pages_management/pages.php
@@ -6,4 +6,30 @@
  */
 ?>
 
-List of static pages from /content/pages folder
+<h4>Pages in <em>/content/pages</em> folder</h4>
+<br/>
+
+    <?php
+        $directory = CONTENT . 'pages';
+
+        if ( ! is_dir($directory)) {
+            exit('Invalid diretory path');
+        }
+
+        $files = array();
+
+        echo '<ul>';
+        foreach (scandir($directory) as $file) {
+            if ('.' === $file) continue;
+            if ('..' === $file) continue;
+
+            $files[] = $file;
+            $page = trim($file, '.php');
+            echo "<li><a href='" . $h->url(array($page)) . "' target='_blank'>" . $page . '</a></li>';
+        }
+        echo '</ul>';
+        
+        //var_dump($files);
+    ?>
+
+
