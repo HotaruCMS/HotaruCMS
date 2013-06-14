2cfec7af106e69bc135296b34fdc92fd5e31b01b
Alan <blog@shibuya246.com>
2013-06-14 19:56:58 +0900

    test start of new autoload func

---
+++ libs/extensions/SmartLoader/autoload.php
@@ -0,0 +1,31 @@
+<?php
+class SimpleClassLoader
+{
+    /**
+     * Installs this class loader on the SPL autoload stack.
+     */
+    public function register()
+    {
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+
+    /**
+     * Uninstalls this class loader from the SPL autoloader stack.
+     */
+    public function unregister()
+    {
+        spl_autoload_unregister(array($this, 'loadClass'));
+    }
+
+    /**
+     * Loads the given class or interface.
+     *
+     * @param string $className The name of the class to load.
+     * @return void
+     */
+    public function loadClass($className)
+    {
+        require str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    }
+}
+?>
