b7679e66942e6261cb0c181b377606dd515f71f5
Alan <blog@shibuya246.com>
2013-06-18 18:46:23 +0900

    add json response http func

--- functions/funcs.http.php
+++ functions/funcs.http.php
@@ -109,4 +109,97 @@ function hotaru_http_request($url)
 	return $body;
 }
 
+
+ function sendResponse($status = 200, $body = '', $content_type = 'text/html', $file = '')
+{
+	// set the status
+	$status_header = 'HTTP/1.1 ' . $status . ' ' . getStatusCodeMessage($status);
+	header($status_header);
+	// and the content type
+	header('Content-type: ' . $content_type);
+        
+        if ($file) {
+            //get the last-modified-date of this very file
+            $lastModified=filemtime($file);
+            //get a unique hash of this file (etag)
+            $etagFile = md5_file($file);
+            
+            //set etag-header
+            header("Etag: $etagFile");
+            //set last-modified header
+            header("Last-Modified: ".gmdate("D, d M Y H:i:s", $lastModified)." GMT");
+        }
+
+	// pages with body are easy
+	if($body != '')
+	{
+	    // send the body
+	    echo $body;
+	    exit;
+	}
+	// we need to create the body if none is passed
+	else
+	{
+	    // create some body messages
+	    $message = '';
+
+	    // this is purely optional, but makes the pages a little nicer to read
+	    // for your users.  Since you won't likely send a lot of different status codes,
+	    // this also shouldn't be too ponderous to maintain
+	    switch($status)
+	    {
+		case 401:
+		    $message = 'You must be authorized to view this page.';
+		    break;
+		case 404:
+		    $message = 'The requested URL ' . $_SERVER['REQUEST_URI'] . ' was not found.';
+		    break;
+		case 500:
+		    $message = 'The server encountered an error processing your request.';
+		    break;
+		case 501:
+		    $message = 'The requested method is not implemented.';
+		    break;
+	    }
+
+	    // servers don't always have a signature turned on
+	    // (this is an apache directive "ServerSignature On")
+	    $signature = ($_SERVER['SERVER_SIGNATURE'] == '') ? $_SERVER['SERVER_SOFTWARE'] . ' Server at ' . $_SERVER['SERVER_NAME'] . ' Port ' . $_SERVER['SERVER_PORT'] : $_SERVER['SERVER_SIGNATURE'];
+
+	    // this should be templated in a real-world solution
+	    $body = '
+		<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+		<html>
+		<head>
+		    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		    <title>' . $status . ' ' . getStatusCodeMessage($status) . '</title>
+		</head>
+		<body>
+		    <h1>' . getStatusCodeMessage($status) . '</h1>
+		    <p>' . $message . '</p>
+		    <hr />
+		    <address>' . $signature . '</address>
+		</body>
+		</html>';
+
+	    echo $body;
+	    exit;
+	}
+    }
+    
+    
+function getStatusCodeMessage($status)
+{
+    $codes = Array(
+        200 => 'OK',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+    );
+    return (isset($codes[$status])) ? $codes[$status] : '';
+}
 ?>
